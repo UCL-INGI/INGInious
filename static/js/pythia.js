5d13f316441dbd7a14d6cc3f49871f11f8caf1aa
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-07-05 15:14:22 +0200

    Tab to spaces

--- static/js/pythia.js
+++ static/js/pythia.js
@@ -1,6 +1,6 @@
 $(function()
 {
-	$('form#task').on('submit', function(){submitTask(); return false;});
+    $('form#task').on('submit', function(){submitTask(); return false;});
 });
 
 //Contains all code editors
@@ -9,13 +9,13 @@ var codeEditors=[]
 //Register and init a code editor (ace)
 function registerCodeEditor(id,lang,lines)
 {
-	var editor = ace.edit(id);
-	if(lang != "plain")
-		editor.getSession().setMode("ace/mode/"+lang);
-	editor.getSession().setTabSize(4);
-	editor.setOptions({minLines: lines,maxLines: lines});
-	
-	var textarea = jQuery('input[name="'+id+'"]');
+    var editor = ace.edit(id);
+    if(lang != "plain")
+        editor.getSession().setMode("ace/mode/"+lang);
+    editor.getSession().setTabSize(4);
+    editor.setOptions({minLines: lines,maxLines: lines});
+    
+    var textarea = jQuery('input[name="'+id+'"]');
     editor.getSession().on("change", function()
     {
         textarea.val(editor.getSession().getValue());
@@ -27,98 +27,98 @@ function registerCodeEditor(id,lang,lines)
 //Blur task form
 function blurTaskForm()
 {
-	for (idx in codeEditors)
-		codeEditors[idx].setReadOnly(true);
-	$("form#task input, form#task button").attr("disabled","disabled");
-	$("form#task").addClass('form-blur');
+    for (idx in codeEditors)
+        codeEditors[idx].setReadOnly(true);
+    $("form#task input, form#task button").attr("disabled","disabled");
+    $("form#task").addClass('form-blur');
 }
 function unblurTaskForm()
 {
-	for (idx in codeEditors)
-		codeEditors[idx].setReadOnly(false);
-	$("form#task input, form#task button").removeAttr("disabled");
-	$("form#task").removeClass('form-blur');
+    for (idx in codeEditors)
+        codeEditors[idx].setReadOnly(false);
+    $("form#task input, form#task button").removeAttr("disabled");
+    $("form#task").removeClass('form-blur');
 }
 
 //Submits a task
 function submitTask()
 {
-	var form = $('form#task');
-	serialized = form.serialize();
-	
-	blurTaskForm();
-	displayTaskLoadingAlert();
-	
-	jQuery.post(form.attr("action"), serialized, null, "json")
-	.done(function(data)
-	{
-		if ("status" in data && data["status"] == "ok" && "jobId" in data)
-		{
-			jobid = data['jobId'];
-			waitForJob(data['jobId']);
-		}
-		else
-		{
-			displayTaskErrorAlert();
-			unblurTaskForm();
-		}
-	})
-	.fail(function()
-	{
-		displayTaskErrorAlert();
-		unblurTaskForm();
-	});
+    var form = $('form#task');
+    serialized = form.serialize();
+    
+    blurTaskForm();
+    displayTaskLoadingAlert();
+    
+    jQuery.post(form.attr("action"), serialized, null, "json")
+    .done(function(data)
+    {
+        if ("status" in data && data["status"] == "ok" && "jobId" in data)
+        {
+            jobid = data['jobId'];
+            waitForJob(data['jobId']);
+        }
+        else
+        {
+            displayTaskErrorAlert();
+            unblurTaskForm();
+        }
+    })
+    .fail(function()
+    {
+        displayTaskErrorAlert();
+        unblurTaskForm();
+    });
 }
 
 //Wait for a job to end
 function waitForJob(jobId)
 {
-	setTimeout(function()
-	{
-		var url = $('form#task').attr("action");
-		jQuery.post(url, {"@action":"check","jobId":jobId}, null, "json")
-		.done(function(data)
-		{
-			if("status" in data && data['status'] == "waiting")
-				waitForJob(jobId);
-			else if("status" in data && data['status'] == "done" && "result" in data)
-			{
-				if(data['result'] == "error" && "text" in data)
-				{
-					displayTaskStudentErrorAlert(data["text"]);
-					unblurTaskForm();
-				}
-				else if(data['result'] == "success")
-				{
-					displayTaskStudentSuccessAlert();
-					unblurTaskForm();
-				}
-				else
-				{
-					displayTaskErrorAlert();
-					unblurTaskForm();
-				}
-			}
-			else
-			{
-				displayTaskErrorAlert();
-				unblurTaskForm();
-			}
-		})
-		.fail(function()
-		{
-			displayTaskErrorAlert();
-			unblurTaskForm();
-		});
-	}, 5000);
+    setTimeout(function()
+    {
+        var url = $('form#task').attr("action");
+        jQuery.post(url, {"@action":"check","jobId":jobId}, null, "json")
+        .done(function(data)
+        {
+            if("status" in data && data['status'] == "waiting")
+                waitForJob(jobId);
+            else if("status" in data && data['status'] == "done" && "result" in data)
+            {
+                if(data['result'] == "error" && "text" in data)
+                {
+                    displayTaskStudentErrorAlert(data["text"]);
+                    unblurTaskForm();
+                }
+                else if(data['result'] == "success")
+                {
+                    displayTaskStudentSuccessAlert();
+                    unblurTaskForm();
+                }
+                else
+                {
+                    displayTaskErrorAlert();
+                    unblurTaskForm();
+                }
+            }
+            else
+            {
+                displayTaskErrorAlert();
+                unblurTaskForm();
+            }
+        })
+        .fail(function()
+        {
+            displayTaskErrorAlert();
+            unblurTaskForm();
+        });
+    }, 5000);
 }
 
 //Displays a loading alert in task form
 function displayTaskLoadingAlert()
 {
-	$('#task_alert').html(getAlertCode("<b>Verifying your answers...</b>","info",false));
-	$('html, body').animate(
-	{
+    $('#task_alert').html(getAlertCode("<b>Verifying your answers...</b>","info",false));
+    $('html, body').animate(
+    {
         scrollTop: $("#task_alert").offset().top-100
     }, 1000);
 }
@@ -126,9 +126,9 @@ function displayTaskLoadingAlert()
 //Displays an internal error alert in task form
 function displayTaskErrorAlert()
 {
-	$('#task_alert').html(getAlertCode("<b>An internal error occured. Please retry later.</b>","danger",true));
-	$('html, body').animate(
-	{
+    $('#task_alert').html(getAlertCode("<b>An internal error occured. Please retry later.</b>","danger",true));
+    $('html, body').animate(
+    {
         scrollTop: $("#task_alert").offset().top-100
     }, 1000);
 }
@@ -136,9 +136,9 @@ function displayTaskErrorAlert()
 //Displays a student error alert in task form
 function displayTaskStudentErrorAlert(content)
 {
-	$('#task_alert').html(getAlertCode("<b>There are some errors in your answer:</b><br/>"+content,"danger",true));
-	$('html, body').animate(
-	{
+    $('#task_alert').html(getAlertCode("<b>There are some errors in your answer:</b><br/>"+content,"danger",true));
+    $('html, body').animate(
+    {
         scrollTop: $("#task_alert").offset().top-100
     }, 1000);
 }
@@ -146,9 +146,9 @@ function displayTaskStudentErrorAlert(content)
 //Displays a student success alert in task form
 function displayTaskStudentSuccessAlert(content)
 {
-	$('#task_alert').html(getAlertCode("<b>Your answer passed the tests!</b>","success",true));
-	$('html, body').animate(
-	{
+    $('#task_alert').html(getAlertCode("<b>Your answer passed the tests!</b>","success",true));
+    $('html, body').animate(
+    {
         scrollTop: $("#task_alert").offset().top-100
     }, 1000);
 }
@@ -158,13 +158,13 @@ function displayTaskStudentSuccessAlert(content)
 //dismissible is a boolean
 function getAlertCode(content,type,dismissible)
 {
-	a = '<div class="alert fade in ';
-	if(dismissible)
-		a += 'alert-dismissible ';
-	a += 'alert-'+type+'" role="alert">';
-	if(dismissible)
-		a += '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>';
-	a += content;
-	a += '</div>';
-	return a;
+    a = '<div class="alert fade in ';
+    if(dismissible)
+        a += 'alert-dismissible ';
+    a += 'alert-'+type+'" role="alert">';
+    if(dismissible)
+        a += '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>';
+    a += content;
+    a += '</div>';
+    return a;
 }
