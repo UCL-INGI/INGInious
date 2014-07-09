5d8b9926da259078911d2a8a01713b076ed58610
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-07-09 21:55:32 +0200

    Improve a bit the container's init script

--- containers/default/pythia.py
+++ containers/default/pythia.py
@@ -1,5 +1,18 @@
 import json
 import sys
+import shutil
+import os
 
+#Copy /ro/task (which is read-only) in /task. Everything will be executed there
+shutil.copytree("/ro/task","/task")
+
+#Change directory to /task
+os.chdir("/task")
+
+#Parse input to return stupid output
 input = json.loads(sys.stdin.readline())
-print json.dumps({"result":"failed","text":"In fact, it's working, but it's a test :D","problems":input})
+problems = {}
+for boxId in input:
+    taskId = boxId.split("/")[0]
+    problems[taskId] = str(input[boxId])
+print json.dumps({"result":"failed","text":"In fact, it's working, but it's a test :D","problems":problems})
