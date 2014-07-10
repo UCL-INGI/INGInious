e9562d70c7247630e3c389f12d81e4d909910eeb
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-07-10 17:23:54 +0200

    Fix some warnings

--- containers/pythia0compat/pythia_input.py
+++ containers/pythia0compat/pythia_input.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Tool to import answer from standard input to the template files given in arguments
 #
 
@@ -14,12 +15,12 @@ input_data = json.loads(sys.stdin.read().strip('\0').strip())
 
 for filepath in sys.argv[1:]:
     filepath = filepath
-    with codecs.open(filepath, 'r', 'utf-8') as file:
-        content = file.read()
+    with codecs.open(filepath, 'r', 'utf-8') as file2:
+        content = file2.read()
     for field in input_data:
         regex = re.compile("@([^@]*)@" + field + '@([^@]*)@')
         for prefix, postfix in set(regex.findall(content)):
             rep = "\n".join([prefix + v + postfix for v in input_data[field].splitlines()])
             content = content.replace("@{0}@{1}@{2}@".format(prefix, field, postfix), rep)
-    with codecs.open(filepath, 'w', 'utf-8') as file:
-        file.write(content)
+    with codecs.open(filepath, 'w', 'utf-8') as file3:
+        file3.write(content)
