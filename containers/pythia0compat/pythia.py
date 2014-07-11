270d2ae2a194dcd968afb35f35726b84636c4430
Guillaume Derval <guillaume@itself.be>
2014-07-11 14:19:37 +0200

    Better handling of timeouts

--- containers/pythia0compat/pythia.py
+++ containers/pythia0compat/pythia.py
@@ -9,6 +9,7 @@ from os import listdir
 from os.path import isfile, join
 import xmltodict
 import tempfile
+import time
 
 def copytree(src, dst, symlinks=False, ignore=None):
     """ Custom copy tree to allow to copy into existing directories """
@@ -21,12 +22,30 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copy2(s, d)
             
 def setlimits():
-    resource.setrlimit(resource.RLIMIT_CPU, (limits["time"], limits["time"]))
+    resource.setrlimit(resource.RLIMIT_CPU, (limits["time"]+5, limits["time"]+5))
     
-
 def setExecutable(filename):
     st = os.stat(filename)
     os.chmod(filename, st.st_mode | stat.S_IEXEC)
+
+def executeProcess(filename,stdinString):
+    setExecutable(filename)
+    stdin = tempfile.TemporaryFile()
+    stdin.write(stdinString)
+    stdin.seek(0)
+    
+    stdout = tempfile.TemporaryFile()
+    stderr = tempfile.TemporaryFile()
+    p = subprocess.Popen([filename], preexec_fn=setlimits, stdin=stdin, stdout=stdout, stderr=stderr)
+    start = time.time()
+    while p.poll() is None:
+        time.sleep(0.2)
+        if time.time()-start > limits["time"]:
+            p.kill()
+            raise Exception("Timeout")
+    stdout.seek(0)
+    stderr.seek(0)
+    return stdout.read(), stderr.read()
 
 #get input data
 stdin = sys.stdin.read().strip('\0').strip()
@@ -69,43 +88,38 @@ for question in input_data:
     open("/tmp/work/output/"+question+".out","w").write(input_data[question])
     
 if os.path.exists("/job/dataset.sh"):
-    setExecutable("/job/dataset.sh")
     os.chdir("/job")
-    stdout = tempfile.TemporaryFile()
-    stderr = tempfile.TemporaryFile()
-    p = subprocess.Popen(["/job/dataset.sh"], preexec_fn=setlimits, stdout=stdout, stderr=stderr)
-    p.wait()
-    stdout.seek(0)
-    stderr.seek(0)
-    stdOutputData["stdout"] = stdOutputData["stdout"]+"DATASET: "+stdout.read()+"\n"
-    stdOutputData["stderr"] = stdOutputData["stderr"]+"DATASET: "+stderr.read()+"\n"
+    try:
+        stdout, stderr = executeProcess("/job/dataset.sh", "")
+    except:
+        print json.dumps({"result":"crash","text":"Dataset.sh did a timeout","problems":{},"v0out":stdOutputData})
+        exit()
+    stdOutputData["stdout"] = stdOutputData["stdout"]+"DATASET: "+stdout+"\n"
+    stdOutputData["stderr"] = stdOutputData["stderr"]+"DATASET: "+stderr+"\n"
 
 os.chdir("/tmp/work")
 if os.path.exists("/job/run.sh"):
     setExecutable("/job/run.sh")
-    stdout = tempfile.TemporaryFile()
-    stderr = tempfile.TemporaryFile()
-    p = subprocess.Popen(["/job/run.sh"], preexec_fn=setlimits, stdout=stdout, stderr=stderr)
-    p.wait()
-    stdout.seek(0)
-    stderr.seek(0)
-    stdOutputData["stdout"] = stdOutputData["stdout"]+"RUN: "+stdout.read()+"\n"
-    stdOutputData["stderr"] = stdOutputData["stderr"]+"RUN: "+stderr.read()+"\n"
+    try:
+        stdout, stderr = executeProcess("/job/run.sh", "")
+    except:
+        print json.dumps({"result":"timeout","text":"Your code did a timeout","problems":{},"v0out":stdOutputData})
+        exit()
+    stdOutputData["stdout"] = stdOutputData["stdout"]+"RUN: "+stdout+"\n"
+    stdOutputData["stderr"] = stdOutputData["stderr"]+"RUN: "+stderr+"\n"
 
 #Move some files
 shutil.copytree("/tmp/work/output","/job/output/files")
 open("/job/output/status","w").write("done")
 
 os.chdir("/job")
-setExecutable("/job/feedback.sh")
-stdout = tempfile.TemporaryFile()
-stderr = tempfile.TemporaryFile()
-p = subprocess.Popen(["/job/feedback.sh"], preexec_fn=setlimits, stdout=stdout, stderr=stderr)
-p.wait()
-stdout.seek(0)
-stderr.seek(0)
-stdOutputData["stdout"] = stdOutputData["stdout"]+"FEEDBACK: "+stdout.read()+"\n"
-stdOutputData["stderr"] = stdOutputData["stderr"]+"FEEDBACK: "+stderr.read()+"\n"
+try:
+    stdout, stderr = executeProcess("/job/feedback.sh", "")
+except:
+    print json.dumps({"result":"crash","text":"Feedback.sh did a timeout","problems":{},"v0out":stdOutputData})
+    exit()
+stdOutputData["stdout"] = stdOutputData["stdout"]+"FEEDBACK: "+stdout+"\n"
+stdOutputData["stderr"] = stdOutputData["stderr"]+"FEEDBACK: "+stderr+"\n"
 
 
 if os.path.exists("feedback.xml"):
