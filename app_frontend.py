86275344e170be0be6183ac87dabfce7379411c7
Anthony Gégo <anthony.gego@student.uclouvain.be>
2014-07-24 16:04:46 +0200

    Resolution of import problems

--- app_frontend.py
+++ app_frontend.py
@@ -4,7 +4,11 @@ import web
 
 from frontend.plugins.plugin_manager import PluginManager
 import common.base
+import frontend
 import frontend.session
+from frontend import submission_manager
+import frontend.base
+
 urls = (
     '/', 'frontend.pages.index.IndexPage',
     '/index', 'frontend.pages.index.IndexPage',
@@ -17,24 +21,24 @@ urls = (
     '/admin/([^/]+)/task/([^/]+)', 'frontend.pages.admin_course.AdminCourseTaskInfoPage',
 )
 
-app = web.application(urls, globals(), autoreload=False)
+app = None
 
-if __name__ == "__main__":
-    common.base.INGIniousConfiguration.load("./configuration.json")
+def init_app(config_file):
+    global app
+    app = web.application(urls, globals(), autoreload=False)
+    common.base.INGIniousConfiguration.load(config_file)
     frontend.session.init(app)
-
-    # Must be done after frontend.session.init(app)
-    import frontend.base
 
     def not_found():
         """ Display the error 404 page """
         return web.notfound(frontend.base.renderer.notfound())
     app.notfound = not_found
-    # Idem
-    import frontend.submission_manager
-    frontend.submission_manager.init_backend_interface()
+
+    submission_manager.init_backend_interface()
 
     # Must be done after everything else
     PluginManager(app, common.base.INGIniousConfiguration.get("plugins", []))
 
+if __name__ == "__main__":
+    init_app("./configuration.json")
     app.run()
--- frontend/user.py
+++ frontend/user.py
@@ -2,7 +2,7 @@
 import sys
 
 from frontend.plugins.plugin_manager import PluginManager
-from frontend.session import session
+import frontend.session
 import frontend.base
 import frontend.user_data
 # Add this module to the templates
@@ -13,45 +13,45 @@ def get_data():
     """ Get the User Data for the connected user """
     if not is_logged_in():
         return None
-    return frontend.user_data.UserData(session.username)
+    return frontend.user_data.UserData(frontend.session.session.username)
 
 
 def get_username():
     """ Returns the username (which is unique) of the current user. Returns None if no user is logged in """
     if not is_logged_in():
         return None
-    return session.username
+    return frontend.session.session.username
 
 
 def get_realname():
     """ Returns the real name of the current user. Returns None if no user is logged in """
     if not is_logged_in():
         return None
-    return session.realname
+    return frontend.session.session.realname
 
 
 def is_logged_in():
     """" Returns if the user is logged in or not """
-    return "loggedin" in session and session.loggedin
+    return "loggedin" in frontend.session.session and frontend.session.session.loggedin
 
 
 def disconnect():
     """ Log off the current user """
-    session.loggedin = False
-    session.username = None
-    session.realname = None
-    session.email = None
+    frontend.session.session.loggedin = False
+    frontend.session.session.username = None
+    frontend.session.session.realname = None
+    frontend.session.session.email = None
     return
 
 
 def connect_user_internal(username, email, realname):
     """ Connect a user. Should only be used by plugins to effectively connect the user. **this function does not make any verifications!** """
-    session.loggedin = True
-    session.email = email
-    session.username = username
-    session.realname = realname
+    frontend.session.session.loggedin = True
+    frontend.session.session.email = email
+    frontend.session.session.username = username
+    frontend.session.session.realname = realname
 
-    get_data().update_basic_informations(session.realname, session.email)
+    get_data().update_basic_informations(frontend.session.session.realname, frontend.session.session.email)
 
 
 def connect(auth_method_id, login_data):
