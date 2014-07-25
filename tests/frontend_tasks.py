42f55353bdd7f687b2f732ea2f9ca305f5516281
Anthony Gégo <anthony.gego@student.uclouvain.be>
2014-07-25 16:44:40 +0200

    Adds multichoice submission tests

--- tests/frontend_tasks.py
+++ tests/frontend_tasks.py
@@ -77,17 +77,12 @@ class frontend_tasks_submit(unittest.TestCase):
     def setUp(self):
         print "\033[1m-> frontend_tasks_submit:setUp\033[0m"
         frontend.session.init(app, {'loggedin':True, 'username':"test", "realname":"Test", "email":"mail@test.com"})
-        
-    def test_code_submit(self):
-        '''Tests submission of a code'''
-        resp = appt.post('/course/test/task1', {"@action":"submit", "unittest/decimal":"2"})
-        js = json.loads(resp.body)
-        assert "status" in js and "submissionid" in js and js["status"] == "ok"
 
-    def test_code_check(self):
+    def test_code(self):
         ''' Tests submission and check of a code '''
         resp = appt.post('/course/test/task1', {"@action":"submit", "unittest/decimal": "2"})
         js = json.loads(resp.body)
+        assert "status" in js and "submissionid" in js and js["status"] == "ok"
         sub_id = js["submissionid"]
         
         for tries in range(0, 100):
@@ -98,26 +93,55 @@ class frontend_tasks_submit(unittest.TestCase):
             if js["status"] == "done":
                 assert "result" in js and js["result"] != "error"
                 break
-       
-    def test_match_submit(self):
-        '''Tests submission of a match'''
-        resp = appt.post('/course/test2/task1', {"@action":"submit", "unittest":"Answer 1"})
-        js = json.loads(resp.body)
-        assert "status" in js and "submissionid" in js and js["status"] == "ok"
     
-    def test_match_check(self):
+    def test_match(self):
         ''' Tests submission and check of a code '''
         resp = appt.post('/course/test2/task1', {"@action":"submit", "unittest": "Answer 1"})
         js = json.loads(resp.body)
+        assert "status" in js and "submissionid" in js and js["status"] == "ok"
         sub_id = js["submissionid"]
         
         for tries in range(0, 100):
             time.sleep(1)
             resp = appt.post('/course/test2/task1', {"@action":"check", "submissionid":sub_id})
             js = json.loads(resp.body)
-            assert "status" in js and "status" != "error"
+            assert "status" in js
             if js["status"] == "done":
-                assert "result" in js and js["result"] != "error"
+                assert "result" in js and js["result"] == "success"
+                break
+    
+    def test_correct_multichoice(self):
+        ''' Tests correct submission and check of a multichoice '''
+        resp = appt.post('/course/test2/task3', {"@action":"submit", "unittest": ["1","0"]})
+        js = json.loads(resp.body)
+        assert "status" in js and "submissionid" in js and js["status"] == "ok"
+        sub_id = js["submissionid"]
+        
+        for tries in range(0, 100):
+            time.sleep(1)
+            resp = appt.post('/course/test2/task3', {"@action":"check", "submissionid":sub_id})
+            js = json.loads(resp.body)
+            assert "status" in js
+            if js["status"] == "done":
+                assert "result" in js and js["result"] == "success"
+                print js
+                break
+
+    def test_incorrect_multichoice(self):
+        ''' Tests incorrect submission and check of a multichoice '''
+        resp = appt.post('/course/test2/task3', {"@action":"submit", "unittest": ["1"]})
+        js = json.loads(resp.body)
+        assert "status" in js and "submissionid" in js and js["status"] == "ok"
+        sub_id = js["submissionid"]
+        
+        for tries in range(0, 100):
+            time.sleep(1)
+            resp = appt.post('/course/test2/task3', {"@action":"check", "submissionid":sub_id})
+            js = json.loads(resp.body)
+            assert "status" in js
+            if js["status"] == "done":
+                assert "result" in js and js["result"] == "failed"
+                print js
                 break
     
     def tearDown(self):
