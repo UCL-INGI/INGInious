fbefe6dc9cb93be48d2a91770ee3dbc8f6964a63
Guillaume Derval <guillaume@itself.be>
2014-07-28 10:41:14 +0200

    Async job submission

--- frontend/plugins/simple_grader.py
+++ frontend/plugins/simple_grader.py
@@ -24,12 +24,85 @@ def init(plugin_manager, config):
 
         The grader will only return fields that are in the job return dict if their key match return_fields.
 
-        External submissions must take the form of a POST request on the url defined by *page_pattern*.
-        This POST must contains two data field:
+        Different types of request are available
 
-        - *taskid*: the task id of the task
+        New synchronized job
+            External submissions must take the form of a POST request on the url defined by *page_pattern*.
+            This POST must contains two data field:
 
-        - *input*: the input for the task, in JSON. The input is a dictionary filled with problemid:problem_answers pairs.
+            - *taskid*: the task id of the task
+
+            - *input*: the input for the task, in JSON. The input is a dictionary filled with problemid:problem_answer pairs.
+
+            The return value will contains the standard return fields of an INGInious backend job plus a "status" field that will
+            contain "ok".
+
+            If an internal error occurs, it will return a dictionary containing
+
+            ::
+
+                {
+                    "status": "error",
+                    "status_message": "A message containing a simple description of the error"
+                }
+
+        New asynchronous job
+            This POST request allows new jobs to be treated asynchronously.
+            It must contains three data fields:
+
+            - *taskid*: the task id of the task
+
+            - *input*: the input for the task, in JSON. The input is a dictionary filled with problemid:problem_answer pairs.
+
+            - *async*: field that indicate that the job must be launched asynchronously. Only have to be present, content is not read.
+
+            The return value will be a dictionnary containing:
+
+            ::
+
+                {
+
+                    "status": "ok",
+                    "jobid": "the jobid of the async job. Will be needed to get the results."
+                }
+
+            or
+
+            ::
+
+                {
+                    "status": "error",
+                    "status_message": "A message describing the error"
+                }
+
+        Get status of asynchronous job
+            Given a jobid in input (as field of the POST request) and will return either:
+
+            ::
+
+                {
+                    "status": "waiting"
+                }
+
+            or
+
+            ::
+
+                {
+                    "status": "error",
+                    "status_message": "A message describing the error"
+                }
+
+            or
+
+            ::
+
+                {
+                    "status": "done",
+                    "...":"..."
+                }
+
+            Where *...* are the results of the job, as defined in the "return_fields" config value.
 
     """
     courseid = config.get('courseid', 'external')
@@ -52,44 +125,62 @@ def init(plugin_manager, config):
                 <body>
                     <form method="post">
                         <textarea style="width:100%; height:400px;" name="input">{"student_response":"{Browse 'Hello World!'}"}</textarea><br/>
-                        <input type="text" name="taskid" value="HelloWorld"/>
+                        <input type="text" name="taskid" value="HelloWorld"/> (taskid)<br/>
+                        <input type="checkbox" name="async"/> async?<br/>
                         <input type="submit"/>
                     </form>
                 </body>
             </html>"""
+
+        def keep_only_config_return_values(self, job_return):
+            return {key: value for key, value in job_return.iteritems() if return_fields.match(key)}
 
         def POST(self):
             """ POST request """
             web.header('Content-Type', 'application/json')
             post_input = web.input()
 
-            if "input" not in post_input or "taskid" not in post_input:
-                return json.dumps({"result": "crash", "text": "The json must contain an input field and a taskid"})
+            if "input" in post_input and "taskid" in post_input:
+                # New job
+                try:
+                    task_input = json.loads(post_input.input)
+                except:
+                    return json.dumps({"status": "error", "status_message": "Cannot decode input"})
 
-            try:
-                task_input = json.loads(post_input.input)
-            except:
-                return json.dumps({"result": "crash", "text": "Cannot decode input"})
+                try:
+                    task = course.get_task(post_input.taskid)
+                except:
+                    return json.dumps({"status": "error", "status_message": "Cannot open task"})
 
-            try:
-                task = course.get_task(post_input.taskid)
-            except:
-                return json.dumps({"result": "crash", "text": "Cannot open task"})
+                if post_input.get("async") is None:
+                    # New sync job
+                    try:
+                        job_semaphore = threading.Semaphore(0)
 
-            try:
-                job_semaphore = threading.Semaphore(0)
+                        def manage_output(a, job):
+                            """ Manages the output of this job """
+                            print "RETURN JOB"
+                            manage_output.jobReturn = job
+                            job_semaphore.release()
+                        frontend.submission_manager.get_backend_job_queue().add_job(task, task_input, manage_output)
+                        job_semaphore.acquire()
+                        job_return = manage_output.jobReturn
+                    except:
+                        return json.dumps({"status": "error", "status_message": "An internal error occured"})
 
-                def manage_output(_, job):
-                    """ Manages the output of this job """
-                    print "RETURN JOB"
-                    manage_output.jobReturn = job
-                    job_semaphore.release()
-                frontend.submission_manager.get_backend_job_queue().add_job(task, task_input, manage_output)
-                job_semaphore.acquire()
-                job_return = manage_output.jobReturn
-            except:
-                return json.dumps({"result": "crash", "text": "An internal error occured"})
-
-            return json.dumps({key: value for key, value in job_return.iteritems() if return_fields.match(key)})
+                    return json.dumps(dict({"status": "ok"}.items() + self.keep_only_config_return_values(job_return).items()))
+                else:
+                    # New async job
+                    jobid = frontend.submission_manager.get_backend_job_queue().add_job(task, task_input, None)
+                    return json.dumps({"status": "ok", "jobid": str(jobid)})
+            elif "jobid" in post_input:
+                # Get status of async job
+                if not frontend.submission_manager.get_backend_job_queue().is_done(post_input["jobid"]):
+                    return json.dumps({"status": "waiting"})
+                else:
+                    job_return = frontend.submission_manager.get_backend_job_queue().get_result(post_input["jobid"])
+                    return json.dumps(dict({"status": "ok"}.items() + self.keep_only_config_return_values(job_return).items()))
+            else:
+                return json.dumps({"status": "error", "status_message": "Unknown request type"})
 
     plugin_manager.add_page(page_pattern, ExternalGrader)
