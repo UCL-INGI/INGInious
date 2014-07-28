811b08a371585c2370183f925df6e1d347f01820
Anthony Gégo <anthony.gego@student.uclouvain.be>
2014-07-28 11:35:02 +0200

    More tests

--- tests/frontend_tasks.py
+++ tests/frontend_tasks.py
@@ -8,7 +8,7 @@ import json
 import time
 from tests import *
 
-class frontend_tasks_display(unittest.TestCase):
+class frontend_tasks(unittest.TestCase):
     def setUp(self):
         print "\033[1m-> frontend_tasks_display:setUp\033[0m"
         frontend.session.init(app, {'loggedin':True, 'username':"test", "realname":"Test", "email":"mail@test.com"})
@@ -72,78 +72,6 @@ class frontend_tasks_display(unittest.TestCase):
     
     def tearDown(self):
         print "\033[1m-> frontend_tasks_display:tearDown\033[0m"
-
-class frontend_tasks_submit(unittest.TestCase):
-    def setUp(self):
-        print "\033[1m-> frontend_tasks_submit:setUp\033[0m"
-        frontend.session.init(app, {'loggedin':True, 'username':"test", "realname":"Test", "email":"mail@test.com"})
-
-    def test_code(self):
-        ''' Tests submission and check of a code '''
-        resp = appt.post('/course/test/task1', {"@action":"submit", "unittest/decimal": "2"})
-        js = json.loads(resp.body)
-        assert "status" in js and "submissionid" in js and js["status"] == "ok"
-        sub_id = js["submissionid"]
-        
-        for tries in range(0, 100):
-            time.sleep(1)
-            resp = appt.post('/course/test/task1', {"@action":"check", "submissionid":sub_id})
-            js = json.loads(resp.body)
-            assert "status" in js and "status" != "error"
-            if js["status"] == "done":
-                assert "result" in js and js["result"] != "error"
-                break
-    
-    def test_match(self):
-        ''' Tests submission and check of a code '''
-        resp = appt.post('/course/test2/task1', {"@action":"submit", "unittest": "Answer 1"})
-        js = json.loads(resp.body)
-        assert "status" in js and "submissionid" in js and js["status"] == "ok"
-        sub_id = js["submissionid"]
-        
-        for tries in range(0, 100):
-            time.sleep(1)
-            resp = appt.post('/course/test2/task1', {"@action":"check", "submissionid":sub_id})
-            js = json.loads(resp.body)
-            assert "status" in js
-            if js["status"] == "done":
-                assert "result" in js and js["result"] == "success"
-                break
-    
-    def test_correct_multichoice(self):
-        ''' Tests correct submission and check of a multichoice '''
-        resp = appt.post('/course/test2/task3', {"@action":"submit", "unittest": ["1","0"]})
-        js = json.loads(resp.body)
-        assert "status" in js and "submissionid" in js and js["status"] == "ok"
-        sub_id = js["submissionid"]
-        
-        for tries in range(0, 100):
-            time.sleep(1)
-            resp = appt.post('/course/test2/task3', {"@action":"check", "submissionid":sub_id})
-            js = json.loads(resp.body)
-            assert "status" in js
-            if js["status"] == "done":
-                assert "result" in js and js["result"] == "success"
-                break
-
-    def test_incorrect_multichoice(self):
-        ''' Tests incorrect submission and check of a multichoice '''
-        resp = appt.post('/course/test2/task3', {"@action":"submit", "unittest": ["1"]})
-        js = json.loads(resp.body)
-        assert "status" in js and "submissionid" in js and js["status"] == "ok"
-        sub_id = js["submissionid"]
-        
-        for tries in range(0, 100):
-            time.sleep(1)
-            resp = appt.post('/course/test2/task3', {"@action":"check", "submissionid":sub_id})
-            js = json.loads(resp.body)
-            assert "status" in js
-            if js["status"] == "done":
-                assert "result" in js and js["result"] == "failed"
-                break
-    
-    def tearDown(self):
-        print "\033[1m-> frontend_tasks_submit:tearDown\033[0m"
 
 if __name__ == "__main__":
     unittest.main()
