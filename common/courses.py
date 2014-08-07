37591ebde300457776c3e685bf2ba2e984709786
Guillaume Derval <guillaume@itself.be>
2014-08-07 15:28:34 +0200

    Move course files. They are now called course.json and are in the folder of each course.

--- common/courses.py
+++ common/courses.py
@@ -17,9 +17,9 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with INGInious.  If not, see <http://www.gnu.org/licenses/>.
 """ Contains the class Course and utility functions """
-from os import listdir
-from os.path import isfile, join, splitext
 import json
+import os
+import os.path
 
 from common.base import INGIniousConfiguration, id_checker
 import common.tasks
@@ -34,15 +34,7 @@ class Course(object):
     @classmethod
     def get_all_courses(cls):
         """Returns a table containing courseid=>Course pairs."""
-        files = [
-            splitext(f)[0] for f in listdir(
-                INGIniousConfiguration["tasks_directory"]) if isfile(
-                join(
-                    INGIniousConfiguration["tasks_directory"],
-                    f)) and splitext(
-                    join(
-                        INGIniousConfiguration["tasks_directory"],
-                        f))[1] == ".course"]
+        files = [os.path.splitext(f)[0] for f in os.listdir(INGIniousConfiguration["tasks_directory"]) if os.path.isfile(os.path.join(INGIniousConfiguration["tasks_directory"], f, "course.json"))]
         output = {}
         for course in files:
             try:
@@ -52,10 +44,10 @@ class Course(object):
         return output
 
     def __init__(self, courseid):
-        """Constructor. courseid is the name of the .course file"""
+        """Constructor. courseid is the name of the the folder containing the file course.json"""
         if not id_checker(courseid):
             raise Exception("Course with invalid name: " + courseid)
-        self._content = json.load(open(join(INGIniousConfiguration["tasks_directory"], courseid + ".course"), "r"))
+        self._content = json.load(open(os.path.join(INGIniousConfiguration["tasks_directory"], courseid, "course.json"), "r"))
         self._id = courseid
         self._tasks_cache = None
 
@@ -69,19 +61,19 @@ class Course(object):
 
     def get_course_tasks_directory(self):
         """Return the complete path to the tasks directory of the course"""
-        return join(INGIniousConfiguration["tasks_directory"], self._id)
+        return os.path.join(INGIniousConfiguration["tasks_directory"], self._id)
 
     def get_tasks(self):
         """Get all tasks in this course"""
         if self._tasks_cache is None:
             # lists files ending with .task in the right directory, and keep only the taskid
             files = [
-                splitext(f)[0] for f in listdir(
-                    self.get_course_tasks_directory()) if isfile(
-                    join(
+                os.path.splitext(f)[0] for f in os.listdir(
+                    self.get_course_tasks_directory()) if os.path.isfile(
+                    os.path.join(
                         self.get_course_tasks_directory(),
-                        f)) and splitext(
-                    join(
+                        f)) and os.path.splitext(
+                    os.path.join(
                         self.get_course_tasks_directory(),
                         f))[1] == ".task"]
             output = {}
