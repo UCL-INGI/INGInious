ea6438fcb3b4fee7cde0eb3e5ec5ad53b6afc3da
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2015-03-19 11:28:51 +0100

    Forgot a print, again

--- frontend/pages/course_admin/student_task.py
+++ frontend/pages/course_admin/student_task.py
@@ -58,7 +58,6 @@ class SubmissionDownloadFeedback(object):
 
     def page(self, course, username, task, submissionid):
         submission = get_submission(submissionid, False)
-        print submission
         if submission["username"] != username or submission["courseid"] != course.get_id() or submission["taskid"] != task.get_id():
             return json.dumps({"status": "error", "text": "You do not have the rights to access to this submission"})
         elif "jobid" in submission:
