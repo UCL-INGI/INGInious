dd0f5545dc4363cfc7cdafeb85140c87d34dc741
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2015-06-30 11:49:20 +0200

    Begin the work on the batch_manager

--- frontend/batch_manager.py
+++ frontend/batch_manager.py
@@ -18,3 +18,44 @@
 # License along with INGInious.  If not, see <http://www.gnu.org/licenses/>.
 """ Manages batch containers """
 
+import frontend.user as User
+from frontend.backend_interface import get_job_manager
+from frontend.configuration import INGIniousConfiguration
+from frontend.base import get_database, get_gridfs
+
+def add_batch_job(course, container_name, inputdata, launcher_name=None, skip_permission=False):
+    """ Add a job in the queue and returns a batch job id """
+
+    jobid = get_job_manager().new_job_id()
+
+    if not skip_permission:
+        if not User.is_logged_in():
+            raise Exception("A user must be logged in to submit an object")
+
+        username = User.get_username()
+        launcher_name = launcher_name or username
+
+        if username not in course.get_admins():
+            raise Exception("The user must be an administrator to start a batch job")
+
+    if container_name not in INGIniousConfiguration.get("batch_containers", []):
+        raise Exception("This batch container is not allowed to be started")
+
+    container_args = get_job_manager().get_batch_container_args(container_name)
+    if container_args is None:
+        raise Exception("This batch container is not available")
+
+
+    obj = {"courseid": course.get_id(), 'container_name': container_name}
+
+    batch_job_id = get_database().batch_jobs.insert(obj)
+
+    launcher_name = launcher_name or "plugin"
+
+    get_job_manager().new_batch_job(container_name, inputdata, batch_job_done_callback, launcher_name="Frontend - {}".format(launcher_name), jobid=None)
+
+    return batch_job_id
+
+def batch_job_done_callback(jobid, result):
+    """ Called when the batch job with id jobid has finished. result is a file-like object pointing to a tar.gz file """
+    pass
