b1d4fb80aed6e9e1325264b536e9bf83565d3e99
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2015-08-04 16:39:31 +0200

    Store sessions in a better way in the LTI frontend

--- frontend/lti/pages/launch.py
+++ frontend/lti/pages/launch.py
@@ -17,79 +17,10 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with INGInious.  If not, see <http://www.gnu.org/licenses/>.
 """ Main page for the LTI provider. Displays a task and allow to answer to it. """
-import base64
-import json
-import mimetypes
-import urllib
 import web
-from frontend.common.task_page_helpers import list_multiple_multiple_choices_and_files, submission_to_json
-from frontend.lti.pages.utils import LTIAuthenticatedPage
+from frontend.lti.pages.utils import LTILaunchPage
 
 
-class LTILaunch(LTIAuthenticatedPage):
-
-    def LTI_GET(self):
-        return self.LTI_POST()
-
-    def LTI_POST(self):
-        userinput = web.input()
-        username = self.user_manager.session_username()
-
-        if "submissionid" in userinput and "questionid" in userinput:
-            # Download a previously submitted file
-            submission = self.submission_manager.get_submission(userinput["submissionid"], True)
-            if submission is None:
-                raise web.notfound()
-            sinput = self.submission_manager.get_input_from_submission(submission, True)
-            if userinput["questionid"] not in sinput:
-                raise web.notfound()
-
-            if isinstance(sinput[userinput["questionid"]], dict):
-                # File uploaded previously
-                mimetypes.init()
-                mime_type = mimetypes.guess_type(urllib.pathname2url(sinput[userinput["questionid"]]['filename']))
-                web.header('Content-Type', mime_type[0])
-                return base64.b64decode(sinput[userinput["questionid"]]['value'])
-            else:
-                # Other file, download it as text
-                web.header('Content-Type', 'text/plain')
-                return sinput[userinput["questionid"]]
-        elif "@action" in userinput and userinput["@action"] == "submit":
-            # Reparse user input with array for multiple choices
-            init_var = list_multiple_multiple_choices_and_files(self.task)
-            userinput = self.task.adapt_input_for_backend(web.input(**init_var))
-
-            if not self.task.input_is_consistent(userinput, self.default_allowed_file_extensions, self.default_max_file_size):
-                web.header('Content-Type', 'application/json')
-                return json.dumps({"status": "error", "text": "Please answer to all the questions. Your responses were not tested."})
-            del userinput['@action']
-
-            # Get debug info if the current user is an admin
-            debug = "Administrator" in self.user_manager.session_roles()
-
-            # Start the submission
-            submissionid = self.submission_manager.add_job(self.task, userinput, debug)
-
-            web.header('Content-Type', 'application/json')
-            return json.dumps({"status": "ok", "submissionid": str(submissionid)})
-        elif "@action" in userinput and userinput["@action"] == "check" and "submissionid" in userinput:
-            if self.submission_manager.is_done(userinput['submissionid']):
-                web.header('Content-Type', 'application/json')
-                result = self.submission_manager.get_submission(userinput['submissionid'])
-                result = self.submission_manager.get_input_from_submission(result)
-                result = self.submission_manager.get_feedback_from_submission(result)
-                return submission_to_json(result, "Administrator" in self.user_manager.session_roles())
-            else:
-                web.header('Content-Type', 'application/json')
-                return json.dumps({'status': "waiting"})
-        elif "@action" in userinput and userinput["@action"] == "load_submission_input" and "submissionid" in userinput:
-            submission = self.submission_manager.get_submission(userinput["submissionid"])
-            submission = self.submission_manager.get_input_from_submission(submission)
-            submission = self.submission_manager.get_feedback_from_submission(submission)
-            if not submission:
-                raise web.notfound()
-            web.header('Content-Type', 'application/json')
-            return submission_to_json(submission, "Administrator" in self.user_manager.session_roles(), True)
-        else:
-            # Display the task itself
-            return self.template_helper.get_renderer().task(self.course, self.task, self.submission_manager.get_user_submissions(self.task))
+class LTILaunchTask(LTILaunchPage):
+    def LAUNCH_POST(self, session_identifier):
+        raise web.seeother('/'+ session_identifier + '/task')
