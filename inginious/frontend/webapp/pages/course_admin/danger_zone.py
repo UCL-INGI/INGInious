4f391875db76253d43c0d97fa0ab3aefe3cc35e2
anthonygego <anthonygego@hotmail.com>
2015-08-28 16:57:16 +0200

    Backup restore

--- inginious/frontend/webapp/pages/course_admin/danger_zone.py
+++ inginious/frontend/webapp/pages/course_admin/danger_zone.py
@@ -33,6 +33,17 @@ from inginious.frontend.webapp.pages.course_admin.utils import INGIniousAdminPag
 class CourseDangerZonePage(INGIniousAdminPage):
     """ Course administration page: list of classrooms """
 
+    def wipe_course(self, courseid):
+        submissions = self.database.submissions.find({"courseid": courseid})
+        for submission in submissions:
+            for key in ["input", "archive"]:
+                if key in submission and type(submission[key]) == bson.objectid.ObjectId:
+                    self.submission_manager.get_gridfs().delete(submission[key])
+
+        self.database.classrooms.remove({"courseid": courseid})
+        self.database.user_tasks.remove({"courseid": courseid})
+        self.database.submissions.remove({"courseid": courseid})
+
     def dump_course(self, courseid):
         """ Create a zip file containing all information about a given course in database and then remove it from db"""
         filepath = os.path.join(self.backup_dir, courseid, datetime.datetime.now().strftime("%Y%m%d.%H%M%S") + ".zip")
@@ -41,13 +52,13 @@ class CourseDangerZonePage(INGIniousAdminPage):
             os.makedirs(os.path.dirname(filepath))
 
         with zipfile.ZipFile(filepath, "w") as zipf:
-            classrooms = self.database.classrooms.find({"courseid": courseid})
+            classrooms = self.database.classrooms.find({"courseid": courseid}, {"_id": 0})
             zipf.writestr("classrooms.json", bson.json_util.dumps(classrooms), zipfile.ZIP_DEFLATED)
 
-            user_tasks = self.database.user_tasks.find({"courseid": courseid})
+            user_tasks = self.database.user_tasks.find({"courseid": courseid}, {"_id": 0})
             zipf.writestr("user_tasks.json", bson.json_util.dumps(user_tasks), zipfile.ZIP_DEFLATED)
 
-            submissions = self.database.submissions.find({"courseid": courseid})
+            submissions = self.database.submissions.find({"courseid": courseid}, {"_id": 0})
             zipf.writestr("submissions.json", bson.json_util.dumps(submissions), zipfile.ZIP_DEFLATED)
 
             submissions.rewind()
@@ -56,15 +67,32 @@ class CourseDangerZonePage(INGIniousAdminPage):
                 for key in ["input", "archive"]:
                     if key in submission and type(submission[key]) == bson.objectid.ObjectId:
                         infile = self.submission_manager.get_gridfs().get(submission[key])
-                        zipf.writestr(os.path.join(key, str(submission[key]) + ".data"), infile.read(), zipfile.ZIP_DEFLATED)
+                        zipf.writestr(key + "/" + str(submission[key]) + ".data", infile.read(), zipfile.ZIP_DEFLATED)
 
-        self.database.classrooms.remove({"courseid": courseid})
-        self.database.user_tasks.remove({"courseid": courseid})
-        self.database.submissions.remove({"courseid": courseid})
+        self.wipe_course(courseid)
 
     def restore_course(self, courseid, backup):
         """ Restores a course of given courseid to a date specified in backup (format : YYYYMMDD.HHMMSS) """
-        pass
+        self.wipe_course(courseid)
+
+        filepath = os.path.join(self.backup_dir, courseid, backup + ".zip")
+        with zipfile.ZipFile(filepath, "r") as zipf:
+
+            classrooms = bson.json_util.loads(zipf.read("classrooms.json"))
+            if len(classrooms) > 0:
+                self.database.classrooms.insert(classrooms)
+
+            user_tasks = bson.json_util.loads(zipf.read("user_tasks.json"))
+            if len(user_tasks) > 0:
+                self.database.user_tasks.insert(user_tasks)
+
+            submissions = bson.json_util.loads(zipf.read("submissions.json"))
+            for submission in submissions:
+                for key in ["input", "archive"]:
+                    if key in submission and type(submission[key]) == bson.objectid.ObjectId:
+                        submission[key] = self.submission_manager.get_gridfs().put(zipf.read(key + "/" + str(submission[key]) + ".data"))
+
+            self.database.submissions.insert(submissions)
 
     def GET(self, courseid):
         """ GET request """
@@ -124,6 +152,7 @@ class CourseDangerZonePage(INGIniousAdminPage):
                     self.restore_course(courseid, data["backupdate"])
                     msg = "Course restored to date : " + dt.strftime("%Y-%m-%d %H:%M:%S") + "."
                 except:
+                    raise
                     msg = "An error occured while restoring backup."
                     error = True
 
