4f570769fbde3dcaca78dd2e6868345354e5a380
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2016-02-11 12:26:01 +0100

    Update the headers of the file, and give the copyright to the right persons

--- inginious/backend/agent/remote_ssh_manager.py
+++ inginious/backend/agent/remote_ssh_manager.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2014 Université Catholique de Louvain.
-#
-# This file is part of INGInious.
-#
-# INGInious is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# INGInious is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with INGInious.  If not, see <http://www.gnu.org/licenses/>.
+# This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
+# more information about the licensing of this file.
+
 """ Agent, managing docker (remote version) """
 import logging
 
