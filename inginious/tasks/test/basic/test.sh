d697b943b8fc39bf15e84a719a698c5c0b7b611c
Anthony Gégo <anthonygego@hotmail.com>
2016-07-20 21:12:40 +0200

    Fix test/basic demonstration task

--- inginious/tasks/test/basic/test.sh
+++ inginious/tasks/test/basic/test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-feedback --result "success"
+feedback --result "success" --feedback "ok, great!"
 
 A=$(getinput q1)
 B=$(getinput q2)
 
-if [ $A != "Vogons" ]
+if [ "$A" != "Vogons" ]
 then
-    echo "Student has given good answer on question 1"
+    echo "Student failed on question 1"
     definetest test1 42
     feedback --result "failed" --feedback "No, $A is not the answer ! Go back to reading !" --id "q1"
     feedback --feedback "It seems you didn't read the book as you should."
 else
     definetest test1 42
-    echo "Student failed on question 1"
+    echo "Student has given good answer on question 1"
 fi
 
-if [ $B != "Ford Prefect" ]
+if [ "$B" != "Ford Prefect" ]
 then
-    echo "Student has given good answer on question 2" 
+    echo "Student failed on question 2" 
     feedback --result "failed" --feedback "No, $B is not Arthur Dent's best friend. Which book did you read ?" --id "q2"
     feedback --feedback "It seems you didn't the book as you should."
 else
-    echo "Student failed on question 2"
+    echo "Student has given good answer on question 2"
 fi
