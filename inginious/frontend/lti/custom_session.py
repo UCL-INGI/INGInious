81558fa645665cd05680af17d5b0488339625ae8
Anthony Gégo <anthonygego@hotmail.com>
2016-11-15 18:30:39 +0100

    Clean and fix custom session data storage

--- inginious/frontend/lti/custom_session.py
+++ inginious/frontend/lti/custom_session.py
@@ -8,36 +8,18 @@ Custom Session Management
 Initial code taken from web.py (public domain)
 """
 
-import os, time, datetime, base64
+import os, time
 import os.path
 from copy import deepcopy
-
-try:
-    import pickle as pickle
-except ImportError:
-    import pickle
-try:
-    import hashlib
-
-    sha1 = hashlib.sha1
-except ImportError:
-    import sha
-
-    sha1 = sha.new
-
+import hashlib
 from web import utils
 import web
-
-
-class SessionExpired(web.HTTPError, object):
-    def __init__(self, message):
-        web.HTTPError.__init__(self, '200 OK', {}, data=message)
 
 
 class CustomSession(object):
     """Session management made via URL. It is a bit different from the default one: you have to call load() manually."""
     __slots__ = [
-        "store", "_initializer", "_last_cleanup_time", "_config", "_data", "session_id", "_killed", "ip",
+        "store", "_initializer", "_last_cleanup_time", "_config", "_data",
         "__getitem__", "__setitem__", "__delitem__"
     ]
 
@@ -47,7 +29,7 @@ class CustomSession(object):
         self._last_cleanup_time = 0
         self._config = utils.storage(web.config.session_parameters)
         self._data = utils.threadeddict()
-        self.session_id = None
+        self._data["session_id"] = None
 
         if app:
             app.add_processor(self._processor)
@@ -82,19 +64,19 @@ class CustomSession(object):
 
     def load(self, session_id):
         """Load the session from the store, by the id given as parameter"""
-        self.session_id = session_id
+        self._data["session_id"] = session_id
 
         # protection against session_id tampering
-        if self.session_id and not self._valid_session_id(self.session_id):
-            self.session_id = None
+        if self._data["session_id"] and not self._valid_session_id(self._data["session_id"]):
+            self._data["session_id"] = None
 
         self._check_expiry()
-        if self.session_id:
-            d = self.store[self.session_id]
+        if self._data["session_id"]:
+            d = self.store[self._data["session_id"]]
             self.update(d)
 
-        if not self.session_id:
-            self.session_id = self._generate_session_id()
+        if not self._data["session_id"]:
+            self._data["session_id"] = self._generate_session_id()
 
             if self._initializer:
                 if isinstance(self._initializer, dict):
@@ -102,16 +84,16 @@ class CustomSession(object):
                 elif hasattr(self._initializer, '__call__'):
                     self._initializer()
 
-        self.ip = web.ctx.ip
+        self._data["ip"] = web.ctx.ip
 
     def _check_expiry(self):
         # check for expiry
-        if self.session_id and self.session_id not in self.store:
-            self.session_id = None
+        if self._data["session_id"] and self._data["session_id"] not in self.store:
+            self._data["session_id"] = None
 
     def _save(self):
-        if not self.get('_killed') and self.session_id is not None:
-            self.store[self.session_id] = dict(self._data)
+        if not self._data.get('_killed') and self._data["session_id"] is not None:
+            self.store[self._data["session_id"]] = dict(self._data)
 
     def _generate_session_id(self):
         """Generate a random id for session"""
@@ -120,7 +102,7 @@ class CustomSession(object):
             rand = os.urandom(16)
             now = time.time()
             secret_key = self._config.secret_key
-            session_id = sha1(("%s%s%s%s" % (rand, now, utils.safestr(web.ctx.ip), secret_key)).encode("utf-8"))
+            session_id = hashlib.sha1(("%s%s%s%s" % (rand, now, utils.safestr(web.ctx.ip), secret_key)).encode("utf-8"))
             session_id = session_id.hexdigest()
             if session_id not in self.store:
                 break
@@ -132,7 +114,7 @@ class CustomSession(object):
 
     def _cleanup(self):
         """Cleanup the stored sessions"""
-        self.session_id = None
+        self._data["session_id"] = None
         current_time = time.time()
         timeout = self._config.timeout
         if current_time - self._last_cleanup_time > timeout:
@@ -141,188 +123,5 @@ class CustomSession(object):
 
     def kill(self):
         """Kill the session, make it no longer available"""
-        del self.store[self.session_id]
-        self._killed = True
-
-
-class Store:
-    """Base class for session stores"""
-
-    def __contains__(self, key):
-        raise NotImplementedError
-
-    def __getitem__(self, key):
-        raise NotImplementedError
-
-    def __setitem__(self, key, value):
-        raise NotImplementedError
-
-    def cleanup(self, timeout):
-        """removes all the expired sessions"""
-        raise NotImplementedError
-
-    def encode(self, session_dict):
-        """encodes session dict as a string"""
-        pickled = pickle.dumps(session_dict)
-        return base64.encodestring(pickled)
-
-    def decode(self, session_data):
-        """decodes the data to get back the session dict """
-        pickled = base64.decodestring(session_data)
-        return pickle.loads(pickled)
-
-
-class DiskStore(Store):
-    """
-    Store for saving a session on disk.
-
-        >>> import tempfile
-        >>> root = tempfile.mkdtemp()
-        >>> s = DiskStore(root)
-        >>> s['a'] = 'foo'
-        >>> s['a']
-        'foo'
-        >>> time.sleep(0.01)
-        >>> s.cleanup(0.01)
-        >>> s['a']
-        Traceback (most recent call last):
-            ...
-        KeyError: 'a'
-    """
-
-    def __init__(self, root):
-        # if the storage root doesn't exists, create it.
-        if not os.path.exists(root):
-            os.makedirs(
-                os.path.abspath(root)
-            )
-        self.root = root
-
-    def _get_path(self, key):
-        if os.path.sep in key:
-            raise ValueError("Bad key: %s" % repr(key))
-        return os.path.join(self.root, key)
-
-    def __contains__(self, key):
-        path = self._get_path(key)
-        return os.path.exists(path)
-
-    def __getitem__(self, key):
-        path = self._get_path(key)
-        if os.path.exists(path):
-            pickled = open(path).read()
-            return self.decode(pickled)
-        else:
-            raise KeyError(key)
-
-    def __setitem__(self, key, value):
-        path = self._get_path(key)
-        pickled = self.encode(value)
-        try:
-            f = open(path, 'w')
-            try:
-                f.write(pickled)
-            finally:
-                f.close()
-        except IOError:
-            pass
-
-    def __delitem__(self, key):
-        path = self._get_path(key)
-        if os.path.exists(path):
-            os.remove(path)
-
-    def cleanup(self, timeout):
-        now = time.time()
-        for f in os.listdir(self.root):
-            path = self._get_path(f)
-            atime = os.stat(path).st_atime
-            if now - atime > timeout:
-                os.remove(path)
-
-
-class DBStore(Store):
-    """Store for saving a session in database
-    Needs a table with the following columns:
-
-        session_id CHAR(128) UNIQUE NOT NULL,
-        atime DATETIME NOT NULL default current_timestamp,
-        data TEXT
-    """
-
-    def __init__(self, db, table_name):
-        self.db = db
-        self.table = table_name
-
-    def __contains__(self, key):
-        data = self.db.select(self.table, where="session_id=$key", vars=locals())
-        return bool(list(data))
-
-    def __getitem__(self, key):
-        now = datetime.datetime.now()
-        try:
-            s = self.db.select(self.table, where="session_id=$key", vars=locals())[0]
-            self.db.update(self.table, where="session_id=$key", atime=now, vars=locals())
-        except IndexError:
-            raise KeyError
-        else:
-            return self.decode(s.data)
-
-    def __setitem__(self, key, value):
-        pickled = self.encode(value)
-        now = datetime.datetime.now()
-        if key in self:
-            self.db.update(self.table, where="session_id=$key", data=pickled, atime=now, vars=locals())
-        else:
-            self.db.insert(self.table, False, session_id=key, atime=now, data=pickled)
-
-    def __delitem__(self, key):
-        self.db.delete(self.table, where="session_id=$key", vars=locals())
-
-    def cleanup(self, timeout):
-        timeout = datetime.timedelta(timeout / (24.0 * 60 * 60))  # timedelta takes numdays as arg
-        last_allowed_time = datetime.datetime.now() - timeout
-        self.db.delete(self.table, where="$last_allowed_time > atime", vars=locals())
-
-
-class ShelfStore:
-    """Store for saving session using `shelve` module.
-
-        import shelve
-        store = ShelfStore(shelve.open('session.shelf'))
-
-    XXX: is shelve thread-safe?
-    """
-
-    def __init__(self, shelf):
-        self.shelf = shelf
-
-    def __contains__(self, key):
-        return key in self.shelf
-
-    def __getitem__(self, key):
-        atime, v = self.shelf[key]
-        self[key] = v  # update atime
-        return v
-
-    def __setitem__(self, key, value):
-        self.shelf[key] = time.time(), value
-
-    def __delitem__(self, key):
-        try:
-            del self.shelf[key]
-        except KeyError:
-            pass
-
-    def cleanup(self, timeout):
-        now = time.time()
-        for k in list(self.shelf.keys()):
-            atime, v = self.shelf[k]
-            if now - atime > timeout:
-                del self[k]
-
-
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod()
+        del self.store[self._data["session_id"]]
+        self._data["_killed"] = True
