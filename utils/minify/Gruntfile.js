c9850df9ab8c8e878e215f1d8318886fdb2a54ac
Guillaume Derval <guillaume@guillaumederval.be>
2017-01-04 09:22:21 +0100

    Fix Grunt for minification of the js files

--- utils/minify/Gruntfile.js
+++ utils/minify/Gruntfile.js
@@ -10,32 +10,32 @@ module.exports = function(grunt)
             },
             dist: {
                 files: {
-                    '../../frontend/webapp/static/js/all-minified.js': [
-                        '../../frontend/common/static/js/libs/jquery.min.js',
-                        '../../frontend/common/static/js/libs/jquery.form.min.js',
-                        '../../frontend/common/static/js/libs/bootstrap.min.js',
-                        '../../frontend/common/static/js/libs/bootstrap-datetimepicker.min.js',
-                        '../../frontend/common/static/js/libs/checked-list-group.js',
-                        '../../frontend/common/static/js/codemirror/codemirror.js',
-                        '../../frontend/common/static/js/codemirror/mode/meta.js',
-                        '../../frontend/common/static/js/common.js',
-                        '../../frontend/common/static/js/task.js',
-                        '../../frontend/webapp/static/js/jquery-sortable.min.js',
-                        '../../frontend/webapp/static/js/webapp.js',
-                        '../../frontend/webapp/static/js/studio.js',
-                        '../../frontend/webapp/static/js/aggregations.js'
+                    '../../inginious/frontend/webapp/static/js/all-minified.js': [
+                        '../../inginious/frontend/common/static/js/libs/jquery.min.js',
+                        '../../inginious/frontend/common/static/js/libs/jquery.form.min.js',
+                        '../../inginious/frontend/common/static/js/libs/bootstrap.min.js',
+                        '../../inginious/frontend/common/static/js/libs/bootstrap-datetimepicker.min.js',
+                        '../../inginious/frontend/common/static/js/libs/checked-list-group.js',
+                        '../../inginious/frontend/common/static/js/codemirror/codemirror.js',
+                        '../../inginious/frontend/common/static/js/codemirror/mode/meta.js',
+                        '../../inginious/frontend/common/static/js/common.js',
+                        '../../inginious/frontend/common/static/js/task.js',
+                        '../../inginious/frontend/webapp/static/js/jquery-sortable.min.js',
+                        '../../inginious/frontend/webapp/static/js/webapp.js',
+                        '../../inginious/frontend/webapp/static/js/studio.js',
+                        '../../inginious/frontend/webapp/static/js/aggregations.js'
                     ],
-                    '../../frontend/lti/static/js/all-minified.js': [
-                        '../../frontend/common/static/js/libs/jquery.min.js',
-                        '../../frontend/common/static/js/libs/jquery.form.min.js',
-                        '../../frontend/common/static/js/libs/bootstrap.min.js',
-                        '../../frontend/common/static/js/libs/bootstrap-datetimepicker.min.js',
-                        '../../frontend/common/static/js/libs/checked-list-group.js',
-                        '../../frontend/common/static/js/codemirror/codemirror.js',
-                        '../../frontend/common/static/js/codemirror/mode/meta.js',
-                        '../../frontend/common/static/js/common.js',
-                        '../../frontend/common/static/js/task.js',
-                        '../../frontend/lti/static/js/lti.js'
+                    '../../inginious/frontend/lti/static/js/all-minified.js': [
+                        '../../inginious/frontend/common/static/js/libs/jquery.min.js',
+                        '../../inginious/frontend/common/static/js/libs/jquery.form.min.js',
+                        '../../inginious/frontend/common/static/js/libs/bootstrap.min.js',
+                        '../../inginious/frontend/common/static/js/libs/bootstrap-datetimepicker.min.js',
+                        '../../inginious/frontend/common/static/js/libs/checked-list-group.js',
+                        '../../inginious/frontend/common/static/js/codemirror/codemirror.js',
+                        '../../inginious/frontend/common/static/js/codemirror/mode/meta.js',
+                        '../../inginious/frontend/common/static/js/common.js',
+                        '../../inginious/frontend/common/static/js/task.js',
+                        '../../inginious/frontend/lti/static/js/lti.js'
                     ]
                 }
             }
@@ -46,8 +46,8 @@ module.exports = function(grunt)
             },
             dist: {
                 files: {
-                    '../../frontend/webapp/static/js/all-minified.js': ['../../frontend/webapp/static/js/all-minified.js'],
-                    '../../frontend/lti/static/js/all-minified.js': ['../../frontend/lti/static/js/all-minified.js']
+                    '../../inginious/frontend/webapp/static/js/all-minified.js': ['../../inginious/frontend/webapp/static/js/all-minified.js'],
+                    '../../inginious/frontend/lti/static/js/all-minified.js': ['../../inginious/frontend/lti/static/js/all-minified.js']
                 }
             }
         }
