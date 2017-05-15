00bb43ce5188f1325f2af97d9789eaedbe5ec6a3
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-05-15 13:40:57 -0500

    Editor with fixed height

--- inginious/frontend/common/static/js/common.js
+++ inginious/frontend/common/static/js/common.js
@@ -103,25 +103,14 @@ function registerCodeEditor(textarea, lang, lines)
         cm.save();
     });
 
-    var min_editor_height = (21 * lines);
-    editor.on("viewportChange", function(cm) { onEditorViewportChange(min_editor_height, cm); });
-    editor.setSize(null, min_editor_height + "px");
-    onEditorViewportChange(min_editor_height, editor); //immediately trigger a size update
+    var max_editor_height = "500";
+    editor.setSize(null, max_editor_height + "px");
 
     if(mode["mode"] != "plain")
         CodeMirror.autoLoadMode(editor, mode["mode"]);
 
     codeEditors.push(editor);
     return editor;
-}
-
-// Verify if the size of each code editor is sufficient
-function onEditorViewportChange(min_editor_height, cm)
-{
-    //if(cm.getScrollInfo()["height"] > min_editor_height)
-    //    cm.setSize(null, "auto");
-    //else
-    cm.setSize(null, "500" + "px");
 }
 
 // Apply parent function recursively
