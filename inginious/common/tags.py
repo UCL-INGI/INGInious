82e5bf5845d1d8a048e44ab94518eab9c9ed9f0c
Olivier Martin <olivier.martin@student.uclouvain.be>
2017-10-18 16:48:11 +0200

    Add the _() function for translations in get_name()

--- inginious/common/tags.py
+++ inginious/common/tags.py
@@ -4,31 +4,32 @@
 # more information about the licensing of this file.
 
 class Tag:
-	""" This class represents a tag. A tag is a kind of badge that represents a concept. """
-    
-	def __init__(self, id, name):
-		self._id = id
-		self._name = name
+    """ This class represents a tag. A tag is a kind of badge that represents a concept. """
 
-	def get_name(self):
-		""" Returns the name of this tag """
-		return self._name
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
 
-	def get_id(self):
-		""" Returns the id of this tag """
-		return self._id
-	
-	@staticmethod
-	def parse_tags_from_string(tag_str):
-		""" Parse a string and return a tag dictionnary by ids of tags """
-		tag_list = []
-		try:
-			if isinstance(tag_str, str) and tag_str != "":
-				for tag in [x.strip() for x in tag_str.split(',')]: # for exemple : ['id1:nom1', 'id2:nom2', …]
-					[id_tag, nom] = tag.split(':') # can cause ValueError
-					if(id_tag != "" and nom != ""):
-						tag_list.append(Tag(id_tag, nom))
-			return tag_list 
-		except ValueError:
-			raise ValueError(_('the tags field is badly formed !'))
-			return []		
+    def get_name(self):
+        """ Returns the name of this tag """
+        return _(self._name)
+
+    def get_id(self):
+        """ Returns the id of this tag """
+        return self._id
+
+    @staticmethod
+    def parse_tags_from_string(tag_str):
+        """ Parse a string and return a tag dictionnary by ids of tags """
+        tag_list = []
+        try:
+            if isinstance(tag_str, str) and tag_str != "":
+                for tag in [x.strip() for x in tag_str.split(',')]: # for exemple : ['id1:nom1', 'id2:nom2', …]
+                    [id_tag, nom] = tag.split(':') # can cause ValueError
+                    if(id_tag != "" and nom != ""):
+                        tag_list.append(Tag(id_tag, nom))
+            return tag_list
+        except ValueError:
+            raise ValueError(_('the tags field is badly formed !'))
+            return []
+            
