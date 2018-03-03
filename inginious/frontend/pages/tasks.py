7e93f220949dc25f7288a38a3e7e7e37ed98d31d
Olivier Martin <olivier.martin@student.uclouvain.be>
2018-03-03 12:10:54 +0100

    Fix bug with input random where username is None when course is open to visitors

--- inginious/frontend/pages/tasks.py
+++ inginious/frontend/pages/tasks.py
@@ -97,11 +97,13 @@ class BaseTaskPage(object):
         self.user_manager.user_saw_task(username, courseid, taskid)
         
         # Generate random inputs and save ot into db
-        random.seed(username + taskid + courseid)
-        random_input_list = [random.random() for i in range(task.get_number_input_random())]
-        self.database.user_tasks.update(
-                {"courseid": task.get_course_id(), "taskid": task.get_id(), "username": username},
-                {"$set": {"random": random_input_list}})
+        random_input_list = []
+        if username is not None:
+            random.seed(username + taskid + courseid)
+            random_input_list = [random.random() for i in range(task.get_number_input_random())]
+            self.database.user_tasks.update(
+                    {"courseid": task.get_course_id(), "taskid": task.get_id(), "username": username},
+                    {"$set": {"random": random_input_list}})
 
         userinput = web.input()
         if "submissionid" in userinput and "questionid" in userinput:
