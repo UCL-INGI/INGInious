27ce39644570bab5d8f4e284f8f8d9d134524fc0
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-03-28 20:01:20 -0500

    Add best submission by verdict details

--- inginious/frontend/plugins/statistics/__init__.py
+++ inginious/frontend/plugins/statistics/__init__.py
@@ -6,6 +6,7 @@ from inginious.frontend.plugins.statistics.pages.course_admin_statistics import 
 from inginious.frontend.plugins.statistics.pages.course_admin_statistics import statistics_course_admin_menu_hook
 from inginious.frontend.plugins.statistics.pages.api.admin.grade_count import GradeCountApi
 from inginious.frontend.plugins.statistics.pages.api.admin.best_submissions_by_verdict import BestSubmissionsByVerdictApi
+from inginious.frontend.plugins.statistics.pages.api.admin.best_submissions_by_verdict_details import BestSubmissionsByVerdictStatisticsDetailApi
 from inginious.frontend.plugins.statistics.pages.api.admin.submissions_by_verdict import SubmissionsByVerdictApi
 from inginious.frontend.plugins.statistics.pages.api.admin.submissions_by_verdict_details import SubmissionsByVerdictDetailsApi
 
@@ -22,6 +23,7 @@ def init(plugin_manager, course_factory, client, config):
     plugin_manager.add_page('/api/stats/admin/grade_count_details', GradeCountDetailsApi)
 
     plugin_manager.add_page('/api/stats/admin/best_submissions_verdict', BestSubmissionsByVerdictApi)
+    plugin_manager.add_page('/api/stats/admin/best_submissions_verdict_details', BestSubmissionsByVerdictStatisticsDetailApi)
 
     plugin_manager.add_page('/api/stats/admin/submissions_verdict', SubmissionsByVerdictApi)
     plugin_manager.add_page('/api/stats/admin/submissions_verdict_details', SubmissionsByVerdictDetailsApi)
