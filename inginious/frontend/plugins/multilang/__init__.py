0ca3ef8bd525794da17ffd4088d2e9f376a4925c
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-05 17:20:26 -0500

    Add languajes list to frontend

--- inginious/frontend/plugins/multilang/__init__.py
+++ inginious/frontend/plugins/multilang/__init__.py
@@ -1,5 +1,9 @@
+import os
+
 from inginious.common.tasks_problems import CodeProblem
 from inginious.frontend.task_problems import DisplayableCodeProblem
+
+PATH_TO_PLUGIN = os.path.abspath(os.path.dirname(__file__))
 
 class CodeMultipleLanguagesProblem(CodeProblem):
     def __init__(self, task, problemid, content, translations=None):
@@ -25,8 +29,18 @@ class DisplayableCodeMultipleLanguagesProblem(CodeMultipleLanguagesProblem, Disp
         "c11": "C11"}
 
     @classmethod
+    def get_renderer(cls, template_helper):
+        """ Get the renderer for this class problem """
+        return template_helper.get_custom_renderer(os.path.join(PATH_TO_PLUGIN, "templates"), False)
+
+    @classmethod
     def get_type_name(cls, gettext):
         return gettext("Code multiple languages")
+
+    @classmethod
+    def show_editbox(cls, template_helper, key):
+        renderer = DisplayableCodeMultipleLanguagesProblem.get_renderer(template_helper)
+        return renderer.multilang_edit(cls._available_languages)
 
 
 def init(plugin_manager, course_factory, client, plugin_config):
