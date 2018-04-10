ab825da19e8ef297bb36dfbb4d30ac745d37c7ab
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-10 18:12:10 -0500

    Fixed superclass calls

--- inginious/frontend/plugins/multilang/__init__.py
+++ inginious/frontend/plugins/multilang/__init__.py
@@ -7,7 +7,7 @@ PATH_TO_PLUGIN = os.path.abspath(os.path.dirname(__file__))
 
 class CodeMultipleLanguagesProblem(CodeProblem):
     def __init__(self, task, problemid, content, translations=None):
-        CodeProblem.__init__(task, problemid, content, translations)
+        CodeProblem.__init__(self, task, problemid, content, translations)
 
     @classmethod
     def get_type(cls):
@@ -16,7 +16,7 @@ class CodeMultipleLanguagesProblem(CodeProblem):
 
 class DisplayableCodeMultipleLanguagesProblem(CodeMultipleLanguagesProblem, DisplayableCodeProblem):
     def __init__(self, task, problemid, content, translations=None):
-        CodeMultipleLanguagesProblem.__init__(task, problemid, content, translations)
+        CodeMultipleLanguagesProblem.__init__(self, task, problemid, content, translations)
 
     _available_languages = {
         "java7": "Java 7",
