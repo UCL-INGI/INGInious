84725e748bffe57f51a354d1da2c1d905d7c35df
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-10 21:47:00 -0500

    Add dropdown setting correctly

--- inginious/frontend/plugins/multilang/static/multilang.js
+++ inginious/frontend/plugins/multilang/static/multilang.js
@@ -1,6 +1,13 @@
 function load_input_code_multiple_languages(submissionid, key, input)
 {
     load_input_code(submissionid, key, input);
+    setDropDownWithTheRightLanguage(key, input[key + "/language"]);
+}
+
+function setDropDownWithTheRightLanguage(key, language)
+{
+    var dropDown = document.getElementById(key + '/language');
+    dropDown.value = language;
 }
 
 function studio_init_template_code_multiple_languages(well, pid, problem)
