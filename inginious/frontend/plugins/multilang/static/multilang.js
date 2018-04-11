e929ec1ca8b127b620427cb9b24dc9bac487c252
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-10 21:20:33 -0500

    Add input loading

--- inginious/frontend/plugins/multilang/static/multilang.js
+++ inginious/frontend/plugins/multilang/static/multilang.js
@@ -1,3 +1,8 @@
+function load_input_code_multiple_languages(submissionid, key, input)
+{
+    load_input_code(submissionid, key, input);
+}
+
 function studio_init_template_code_multiple_languages(well, pid, problem)
 {
     if("type" in problem)
