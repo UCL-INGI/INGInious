39ab7d0c9d8ef9a7d8961ed12b3537eee21a6364
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-12 22:47:35 -0500

    Add grading result showing

--- inginious/frontend/plugins/multilang/__init__.py
+++ inginious/frontend/plugins/multilang/__init__.py
@@ -66,5 +66,6 @@ def init(plugin_manager, course_factory, client, plugin_config):
     plugin_manager.add_page(r'/multilang/static/(.*)', create_static_resource_page(_static_folder_path))
     plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/multilang.js")
     plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/pythonTutor.js")
+    plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/grader.js")
     plugin_manager.add_hook("css", lambda: "/multilang/static/multilang.css")
     course_factory.get_task_factory().add_problem_type(DisplayableCodeMultipleLanguagesProblem)
