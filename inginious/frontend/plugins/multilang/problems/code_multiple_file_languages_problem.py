65a216e6d901c223e02c994122e01ac302819b13
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-24 13:45:40 -0500

    Add first spec

---
+++ inginious/frontend/plugins/multilang/problems/code_multiple_file_languages_problem.py
@@ -0,0 +1,49 @@
+import os
+
+from inginious.common.tasks_problems import FileProblem
+from inginious.frontend.task_problems import DisplayableFileProblem
+from .languages import available_languages
+
+path_to_plugin = os.path.abspath(os.path.dirname(__file__))
+
+class CodeFileMultipleLanguagesProblem (FileProblem):
+    """Code file problem with multiple languages"""
+
+    def __init__(self, task, problemid, content, translations=None):
+        FileProblem.__init__(self, task, problemid, content, translations)
+        self._languages = content["languages"]
+
+    @classmethod
+    def get_type(cls):
+        return "code_file_multiple_languages"
+
+class DisplayableCodeFileMultipleLanguagesProblem(CodeFileMultipleLanguagesProblem, DisplayableFileProblem):
+    """ A displayable code file problem with multiple languages """
+
+    def __init__(self, task, problemid, content, translations=None):
+        CodeFileMultipleLanguagesProblem.__init__(self, task, problemid, content, translations)
+
+    @classmethod
+    def get_renderer(cls, template_helper):
+        """ Get the renderer for this class problem """
+        return template_helper.get_custom_renderer(os.path.join(path_to_plugin, "templates"), False)
+
+    @classmethod
+    def get_type_name(cls, gettext):
+        return gettext("code file multiple languages")
+
+    @classmethod
+    def show_editbox(cls, template_helper, key):
+        renderer = DisplayableCodeFileMultipleLanguagesProblem.get_renderer(template_helper)
+        return renderer.multilang_edit(key, available_languages())
+
+    # def show_input(self, renderer):
+    #     allowed_languages = {language: self._available_languages[language] for language in self._languages}
+    #     dropdown_id = self.get_id() + "/language"
+    #     custom_input_id = self.get_id() + "/input"
+
+    #     standard_code_file_problem_render = super(DisplayableCodeFileMultipleLanguagesProblem, self).show_input(renderer)
+    #     multiple_language_render = str(renderer.tasks.multiplelanguage(self.get_id(), dropdown_id, allowed_languages, self.get_id(), self.get_type()))
+    #     tools_render = str(renderer.tasks.tools(self.get_id(), "plain", custom_input_id, self.get_type()))
+
+    #     return multiple_language_render + standard_code_file_problem_render + tools_render
