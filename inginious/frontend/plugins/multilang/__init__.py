26bdc375e603c897eeb44b04ea9bde6881973f49
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-04-27 15:56:29 -0500

    Add plugin parametrization

--- inginious/frontend/plugins/multilang/__init__.py
+++ inginious/frontend/plugins/multilang/__init__.py
@@ -3,7 +3,7 @@ import os
 from inginious.frontend.plugins.utils import create_static_resource_page
 from inginious.frontend.plugins.multilang.problems.code_multiple_languages_problem import DisplayableCodeMultipleLanguagesProblem
 from inginious.frontend.plugins.multilang.problems.code_multiple_file_languages_problem import DisplayableCodeFileMultipleLanguagesProblem
-
+from inginious.frontend.plugins.multilang.problems.constants import set_linter_url, set_python_tutor_url
 
 _static_folder_path = os.path.join(os.path.dirname(__file__), "static")
 
@@ -18,3 +18,11 @@ def init(plugin_manager, course_factory, client, plugin_config):
     plugin_manager.add_hook("css", lambda: "/multilang/static/lint.css")
     course_factory.get_task_factory().add_problem_type(DisplayableCodeMultipleLanguagesProblem)
     course_factory.get_task_factory().add_problem_type(DisplayableCodeFileMultipleLanguagesProblem)
+
+    python_tutor_url = plugin_config.get("python_tutor_url", "")
+    if python_tutor_url != "":
+        set_python_tutor_url(python_tutor_url)
+
+    linter_url = plugin_config.get("linter_url", "")
+    if linter_url != "":
+        set_linter_url(linter_url)
