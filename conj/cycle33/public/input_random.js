19030e3ccae66e0a5fc29e5b2bcc9023683e27b7
naitali <brandon.naitali@student.uclouvain.be>
2018-11-09 11:21:27 +0100

    Rename course + tasks

---
+++ conj/cycle33/public/input_random.js
@@ -0,0 +1,11 @@
+$.getJSON("/course/conj/cycle33/file.json", function(data) {
+
+//data is the JSON string
+var i = parseInt(input["@random"][0] * data.random_tab.length);  
+document.getElementById("verb").innerHTML = data.random_tab[i];
+var j = parseInt(input["@random"][1] * data.random_tab_person.length);    
+document.getElementById("pers").innerHTML = data.random_tab_person[j];
+var k = parseInt(input["@random"][2] * data.random_tab_tenses.length);    
+document.getElementById("tense").innerHTML = data.random_tab_tenses[k];
+});
+    
