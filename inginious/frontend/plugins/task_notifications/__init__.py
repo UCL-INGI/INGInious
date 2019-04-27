0eaf63c1579370f2da8fb9b84eac1ac574939ac5
sprt <hellosprt@gmail.com>
2019-04-27 19:42:20 +0200

    Include the INGInious logo in task notifications

--- inginious/frontend/plugins/task_notifications/__init__.py
+++ inginious/frontend/plugins/task_notifications/__init__.py
@@ -12,7 +12,8 @@ _script = """
 let params = {params_obj};
 if (localStorage.getItem("taskNotifications") === "on") {{
     new Notification(params.taskName, {{
-        body: `Task finished (${{params.result}}). Your score is ${{params.grade}}%.`
+        body: `Task finished (${{params.result}}). Your score is ${{params.grade}}%.`,
+        icon: "/static/images/header.png",
     }});
 }}
 """
