bcee167c7dd392a6d2eb4a51d6aed06457a3f8ef
sprt <hellosprt@gmail.com>
2019-04-28 12:01:18 +0200

    Task notifications: explain feature the first time
    This sends a notification explaining how the feature works the first
    time it is enabled.

--- inginious/frontend/static/plugins/task_notifications/task_notifications.js
+++ inginious/frontend/static/plugins/task_notifications/task_notifications.js
@@ -8,16 +8,28 @@ notificationCheckbox.checked = notificationsEnabled;
 notificationCheckbox.addEventListener("change", function (e) {
     if (e.target.checked) {
         if (!("Notification" in window)) {
+            e.target.checked = false;
             alert("This browser does not support notifications.");
             return;
         }
 
         Notification.requestPermission().then(function (permission) {
             if (permission === "granted") {
+                if (localStorage.getItem("taskNotifications") === null) {
+                    new Notification("Task notifications are now enabled", {
+                        body: "You will now receive a notification any time a task you have submitted has finished running. " +
+                              "The notification will only display if you are not currently looking at the task's tab. " +
+                              "\n\n" +
+                              "This explanation will only show up once.",
+                        icon: "/static/images/header.png",
+                    });
+                }
+
                 localStorage.setItem("taskNotifications", "on");
             } else {
                 e.target.checked = false;
-                alert("Your browser has not granted permission to INGInious to send you notifications. Please check your browser's settings.");
+                alert("Your browser has not granted INGInious the permission to send you notifications. " +
+                      "Please check your browser's settings.");
             }
         });
     } else {
