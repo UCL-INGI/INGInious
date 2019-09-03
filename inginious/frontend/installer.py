0281761154573afa2c14dd1af5c4a4a098383d86
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2019-09-03 14:06:55 +0200

    document installer changes

--- inginious/frontend/installer.py
+++ inginious/frontend/installer.py
@@ -272,6 +272,7 @@ class Installer:
         """ Try MongoDB configuration """
         try:
             mongo_client = MongoClient(host=host)
+            # Effective access only occurs when we call a method on the connexion
             mongo_version = str(mongo_client.server_info()['version'])
             self._display_info("Found mongodb server running version %s on %s." % (mongo_version, host))
         except Exception as e:
@@ -280,12 +281,14 @@ class Installer:
 
         try:
             database = mongo_client[database_name]
+            # Effective access only occurs when we call a method on the database.
             database.list_collection_names()
         except Exception as e:
             self._display_warning("Cannot access database %s: %s" % (database_name, str(e)))
             return None
 
         try:
+            # Effective access only occurs when we call a method on the grifs object.
             GridFS(database).find_one()
         except Exception as e:
             self._display_warning("Cannot access gridfs %s: %s" % (database_name, str(e)))
