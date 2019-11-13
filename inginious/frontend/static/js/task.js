2f47f5f6dc8fc549ce9fcd2bc2b69920416ddff9
Drumor <ludovic.taffin@uclouvain.be>
2019-11-13 16:30:08 +0100

    basic localstorage implementation - Need review

--- inginious/frontend/static/js/task.js
+++ inginious/frontend/static/js/task.js
@@ -925,3 +925,85 @@ function updateTagsToNewSubmission(elem, data){
         badge.attr('data-original-title', tags_ok.join(", "));
     }
 }
+
+function load_from_storage(courseid,taskid){
+    if (typeof(Storage) !== "undefined") {
+        for(var problemid in problems_types) {
+              window["load_webstorage_" + problems_types[problemid]](courseid,taskid,problemid);
+        }
+    } else {
+      // Sorry! No Web Storage support..
+        console.log("Your browser doesn't support web storage");
+    }
+
+
+}
+
+function save_to_storage(courseid,taskid){
+    if (typeof(Storage) !== "undefined") {
+        for(var problemid in problems_types) {
+              try{
+                  window["save_webstorage_" + problems_types[problemid]](courseid,taskid,problemid);
+              }catch(error) {
+                console.error(error);
+                // expected output: ReferenceError: nonExistentFunction is not defined
+                }
+
+        }
+
+    } else {
+      // Sorry! No Web Storage support..
+        console.log("Your browser doesn't support web storage");
+    }
+}
+function save_webstorage_code(courseid,taskid,problemid){
+        var saved_name = courseid+"/"+taskid+"/"+problemid;
+        localStorage.setItem(saved_name, codeEditors[problemid].getTextArea().value);
+}
+
+function load_webstorage_code(courseid,taskid,problemid){
+
+       codeEditors[problemid].setValue(localStorage[courseid+"/"+taskid+"/"+problemid]);
+}
+function save_webstorage_multiple_choice(courseid,problemid){
+    $(".problem input[name='" + problemid + "']").each(function(){
+        if(this.checked){
+            var name = courseid+"/"+taskid+"/"+($(this).attr("id"));
+            localStorage.setItem(name, "true");
+
+        }
+    });
+
+}
+
+function load_webstorage_multiple_choice(courseid,taskid,problemid){
+    $(".problem input[name='" + problemid + "']").each(function(){
+        var name = courseid+"/"+taskid+"/"+($(this).attr("id"));
+        if(localStorage[name]==="true"){
+            this.checked=true;
+        }
+    });
+
+}
+
+function save_webstorage_code_single_line(courseid,taskid,problemid){
+    var elem = $(".problem input[name='" + problemid + "']");
+    var name = courseid+"/"+taskid+"/"+(elem.attr("name"));
+    localStorage.setItem(name,elem.val());
+
+}
+function save_webstorage_match(courseid,taskid,problemid){
+    save_webstorage_code_single_line(courseid,problemid);
+
+}
+
+function load_webstorage_code_single_line(courseid,taskid,problemid){
+    var elem = $(".problem input[name='" + problemid + "']");
+    var name = courseid+"/"+taskid+"/"+(elem.attr("name"));
+    elem.val(localStorage[name]);
+
+}
+function load_webstorage_match(courseid,taskid,problemid){
+    load_webstorage_code_single_line(courseid,problemid);
+
+}
