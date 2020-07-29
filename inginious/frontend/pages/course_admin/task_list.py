ff54d637a4fa80b71bcc2557f6b2785afe74d523
bensim602 <bensim.602@gmail.com>
2020-07-29 16:03:07 +0200

    [Course structure]: Add structure to tasks tab and remove stats (move in previous commit) and order (replace by drag and drop in future commits)

--- inginious/frontend/pages/course_admin/task_list.py
+++ inginious/frontend/pages/course_admin/task_list.py
@@ -2,12 +2,9 @@
 #
 # This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
 # more information about the licensing of this file.
-
 from collections import OrderedDict
 
-import web
-
-from inginious.frontend.pages.course_admin.utils import make_csv, INGIniousAdminPage
+from inginious.frontend.pages.course_admin.utils import INGIniousAdminPage
 
 
 class CourseTaskListPage(INGIniousAdminPage):
@@ -15,24 +12,12 @@ class CourseTaskListPage(INGIniousAdminPage):
 
     def GET_AUTH(self, courseid):  # pylint: disable=arguments-differ
         """ GET request """
-        course, __ = self.get_course_and_check_rights(courseid)
+        course, __ = self.get_course_and_check_rights(courseid, allow_all_staff=False)
         return self.page(course)
 
     def POST_AUTH(self, courseid):  # pylint: disable=arguments-differ
         """ POST request """
-        course, __ = self.get_course_and_check_rights(courseid)
-        data = web.input(task=[])
-
-        if "task" in data:
-            # Change tasks order
-            for index, taskid in enumerate(data["task"]):
-                try:
-                    task = self.task_factory.get_task_descriptor_content(courseid, taskid)
-                    task["order"] = index
-                    self.task_factory.update_task_descriptor_content(courseid, taskid, task)
-                except:
-                    pass
-
+        course, __ = self.get_course_and_check_rights(courseid, allow_all_staff=False)
         return self.page(course)
 
     def submission_url_generator(self, taskid):
@@ -41,26 +26,6 @@ class CourseTaskListPage(INGIniousAdminPage):
 
     def page(self, course):
         """ Get all data and display the page """
-        data = list(self.database.user_tasks.aggregate(
-            [
-                {
-                    "$match":
-                        {
-                            "courseid": course.get_id(),
-                            "username": {"$in": self.user_manager.get_course_registered_users(course, False)}
-                        }
-                },
-                {
-                    "$group":
-                        {
-                            "_id": "$taskid",
-                            "viewed": {"$sum": 1},
-                            "attempted": {"$sum": {"$cond": [{"$ne": ["$tried", 0]}, 1, 0]}},
-                            "attempts": {"$sum": "$tried"},
-                            "succeeded": {"$sum": {"$cond": ["$succeeded", 1, 0]}}
-                        }
-                }
-            ]))
 
         # Load tasks and verify exceptions
         files = self.task_factory.get_readable_tasks(course)
@@ -73,18 +38,10 @@ class CourseTaskListPage(INGIniousAdminPage):
                 errors.append({"taskid": task, "error": str(inst)})
         tasks = OrderedDict(sorted(list(output.items()), key=lambda t: (t[1].get_order(), t[1].get_id())))
 
-        # Now load additional information
-        result = OrderedDict()
+        tasks_data = OrderedDict()
         for taskid in tasks:
-            result[taskid] = {"name": tasks[taskid].get_name(self.user_manager.session_language()), "viewed": 0, "attempted": 0, "attempts": 0, "succeeded": 0,
+            tasks_data[taskid] = {"name": tasks[taskid].get_name(self.user_manager.session_language()),
                               "url": self.submission_url_generator(taskid)}
-        for entry in data:
-            if entry["_id"] in result:
-                result[entry["_id"]]["viewed"] = entry["viewed"]
-                result[entry["_id"]]["attempted"] = entry["attempted"]
-                result[entry["_id"]]["attempts"] = entry["attempts"]
-                result[entry["_id"]]["succeeded"] = entry["succeeded"]
-        if "csv" in web.input():
-            return make_csv(result)
 
-        return self.template_helper.get_renderer().course_admin.task_list(course, result, errors, self.webdav_host)
+        return self.template_helper.get_renderer().course_admin.task_list(course, course.get_toc(), tasks_data, errors)
+
