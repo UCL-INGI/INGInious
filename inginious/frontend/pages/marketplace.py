e3d63f06267c6a20b16d289eafb1d56d8a8a6cab
bensim602 <bensim.602@gmail.com>
2020-08-06 16:37:24 +0200

    [Marketplace]: Add content to MarketPlace page

--- inginious/frontend/pages/marketplace.py
+++ inginious/frontend/pages/marketplace.py
@@ -6,18 +6,12 @@
 """ Course page """
 import web
 
+from inginious.frontend.marketplace_courses import get_all_marketplace_courses, get_marketplace_course
 from inginious.frontend.pages.utils import INGIniousAuthPage
 
 
 class Marketplace(INGIniousAuthPage):
     """ Course marketplace """
-
-    def POST_AUTH(self):  # pylint: disable=arguments-differ
-        """ POST request """
-        # Change to teacher privilege when created
-        if not self.user_manager.user_is_superadmin():
-            raise web.notfound()
-        return self.show_page()
 
     def GET_AUTH(self):  # pylint: disable=arguments-differ
         """ GET request """
@@ -26,6 +20,16 @@ class Marketplace(INGIniousAuthPage):
             raise web.notfound()
         return self.show_page()
 
+    def POST_AUTH(self):  # pylint: disable=arguments-differ
+        """ POST request """
+        # Change to teacher privilege when created
+        if not self.user_manager.user_is_superadmin():
+            raise web.notfound()
+        return self.show_page()
+
     def show_page(self):
         """ Prepares and shows the course marketplace """
-        return self.template_helper.get_renderer().marketplace()
+        if errors is None:
+            errors = []
+        courses = get_all_marketplace_courses()
+        return self.template_helper.get_renderer(use_jinja=True).marketplace(courses=courses, errors=errors)
