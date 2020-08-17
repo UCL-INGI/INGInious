dd27d9bed27eac5179b92251446225ba39cc526d
bensim602 <bensim.602@gmail.com>
2020-08-17 12:04:30 +0200

    [Submissions]: Fix for users without name

--- inginious/frontend/pages/course_admin/utils.py
+++ inginious/frontend/pages/course_admin/utils.py
@@ -74,7 +74,7 @@ class INGIniousSubmissionsAdminPage(INGIniousAdminPage):
 
     def get_users(self, course):
         user_ids = self.user_manager.get_course_registered_users(course)
-        users = {user: self.user_manager.get_user_realname(user) for user in user_ids}
+        users = {user: self.user_manager.get_user_realname(user) or '' for user in user_ids}
         return OrderedDict(sorted(users.items(), key=lambda x: x[1]))
 
     def get_input_params(self, user_input, course, limit=50):
