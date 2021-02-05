f7260109269d1c55e02d97cfe49d4770d5811def
Anthony Gégo <anthonygego@hotmail.com>
2021-02-05 12:06:54 +0100

    jinja: migrate course_admin/audience_edit.html

--- inginious/frontend/pages/course_admin/audience_edit.py
+++ inginious/frontend/pages/course_admin/audience_edit.py
@@ -49,9 +49,9 @@ class CourseEditAudience(INGIniousAdminPage):
             raise self.app.notfound(message=_("This audience doesn't exist."))
 
         student_list, tutor_list, other_students, users_info = self.get_user_lists(course, audienceid)
-        return self.template_helper.get_renderer().course_admin.audience_edit(course, student_list, tutor_list,
-                                                                                   other_students, users_info,
-                                                                                   audience, msg, error)
+        return self.template_helper.render("course_admin/audience_edit.html", course=course, student_list=student_list,
+                                           tutor_list=tutor_list,other_students=other_students, users_info=users_info,
+                                           audience=audience, msg=msg, error=error)
 
     def GET_AUTH(self, courseid, audienceid):  # pylint: disable=arguments-differ
         """ Edit a audience """
