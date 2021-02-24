dde0e010ab14de42f4a272a9bf8351a3d193b78f
Anthony Gégo <anthonygego@hotmail.com>
2021-02-24 12:41:44 +0100

    l10n_manager: check if flask has context before accessing session

--- inginious/frontend/l10n_manager.py
+++ inginious/frontend/l10n_manager.py
@@ -4,17 +4,17 @@
 # more information about the licensing of this file.
 
 import gettext
-from flask import session as flask_session
+import flask
 
 class L10nManager:
 
     def __init__(self):
         self.translations = {}
-        self._session = flask_session
+        self._session = flask.session
 
     def get_translation_obj(self, lang=None):
         if lang is None:
-            lang = self._session.get("language", "")
+            lang = self._session.get("language", "") if flask.has_app_context() else ""
         return self.translations.get(lang, gettext.NullTranslations())
 
     def gettext(self, *args, **kwargs):
