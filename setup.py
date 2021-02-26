f7c3fba82f4c6238680f006fc2b18033a466b41b
Guillaume Derval <guillaume.derval@uclouvain.be>
2021-02-26 22:02:48 +0100

    Dataclasses are not available in 3.6.x

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
     "Flask >= 1.1.0",
     "Flask-Mail >= 0.9.1",
     "importlib_metadata >= 3.7.0",
-    'dataclasses >= 0.8; python_version < "3.6.0"'
+    'dataclasses >= 0.8; python_version < "3.7.0"'
 ]
 
 test_requires = [
