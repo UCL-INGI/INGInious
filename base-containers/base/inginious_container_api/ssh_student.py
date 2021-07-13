11f93716d1283f1afab1cf784c94fa1d22d6fb8d
darkyne <gregory.creupelandt@uclouvain.be>
2021-07-13 10:47:20 +0200

    closure_script renamed teardown_script

--- base-containers/base/inginious_container_api/ssh_student.py
+++ base-containers/base/inginious_container_api/ssh_student.py
@@ -7,7 +7,7 @@ import inginious_container_api.run_student
 
 
 # Simply runs run_student with no cmd and ssh set to True
-def ssh_student(setup_script=None, memory_limit=0, hard_time_limit=0, time_limit=0, container=None, run_as_root=False, closure_script=None):
+def ssh_student(setup_script=None, memory_limit=0, hard_time_limit=0, time_limit=0, container=None, run_as_root=False, teardown_script=None):
     """
     If a command is specified, runs it in a student container.
     Then gives ssh access to that container to the student. The student has 2 min to connect (copy paste the ssh command shown) otherwise it will timeout.
@@ -24,9 +24,9 @@ def ssh_student(setup_script=None, memory_limit=0, hard_time_limit=0, time_limit
     :param container: container to use. Must be present in the current agent. By default it is None, meaning the current
                       container type will be used.
     :param run_as_root: If set to True, try to give root access to the student via ssh. Default is False. This feature is still in Beta and should not be used for now.
-    :param closure_script: command to be ran (as a string, with parameters) in the student container after the student closed the ssh session.
+    :param teardown_script: command to be ran (as a string, with parameters) in the student container after the student closed the ssh session.
     """
     return inginious_container_api.run_student.run_student(cmd=setup_script, container=container, time_limit=time_limit,
                 hard_time_limit=hard_time_limit, memory_limit=memory_limit,
                 stdin=sys.stdin.fileno(), stdout=sys.stdout.fileno(), stderr=sys.stderr.fileno(),
-                signal_handler_callback=inginious_container_api.run_student._hack_signals, ssh=True, run_as_root=run_as_root, closure_script=closure_script)
+                signal_handler_callback=inginious_container_api.run_student._hack_signals, ssh=True, run_as_root=run_as_root, teardown_script=teardown_script)
