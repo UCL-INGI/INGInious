4c605db920ce21a74432589655b594fe1ca2e94a
darkyne <gregory.creupelandt@uclouvain.be>
2021-09-22 10:52:37 +0200

    building images doc

--- base-containers/base/inginious_container_api/run_student.py
+++ base-containers/base/inginious_container_api/run_student.py
@@ -23,8 +23,7 @@ def run_student(cmd, container=None,
     Run a command inside a student container
 
     :param cmd: command to be ran (as a string, with parameters). If ssh is set to True, this command will be run before launching the ssh server.
-    :param container: container to use. Must be present in the current agent. By default it is None, meaning the current
-                      container type will be used.
+    :param container: container to use. Must be present in the current agent. By default it is None, meaning the current container type will be used.
     :param time_limit: time limit in seconds. By default it is 0, which means that it will be the same as the current
                        container (NB: it does not count in the "host" container timeout!)
     :param hard_time_limit: hard time limit. By default it is 0, which means that it will be the same as the current
