9634da582b030a6ae228cd8c8a62bdcc9122bf55
GenMiha <michail.bogomolov@gmail.com>
2021-12-22 11:49:38 +0300

    genmihatests.py and unittest.py move.genmihatests2.py created. report.html created

---
+++ submissions/genmihatests.py
@@ -0,0 +1,22 @@
+def char_counter(word, character):
+    counter = 0
+    for letters in word:
+        if letters[i] == character
+            counter += 1
+    return counter
+    
+    
+def factorial(n):
+    if n == 1
+        return 1
+    return n*factorial(n-1)
+    
+    
+def fibonachi(n):
+    if n in (1, 2)
+        return 1
+    return fibonachi(n-1) + fibonachi(n-2)
+    
+    
+    
+    
---
+++ submissions/genmihatests2.py
@@ -0,0 +1,12 @@
+def char_counter(word, character):
+    counter = 0
+    for letters in word:
+        if letters[i] == character
+            counter += 1
+    return counter
+    
+def fibonachi(n):
+    if n in (1, 2)
+        return 1
+    return fibonachi(n-1) + fibonachi(n-2)
+    
