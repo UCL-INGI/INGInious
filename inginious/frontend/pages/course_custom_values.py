b614974e2d4bce49a6876be3c544da50bb3a07ad
Ludovic Taffin <ludovic.taffin@uclouvain.be>
2022-07-06 13:52:09 +0200

    add data to database + displays

--- inginious/frontend/pages/course_custom_values.py
+++ inginious/frontend/pages/course_custom_values.py
@@ -9,17 +9,32 @@ from inginious.frontend.pages.utils import INGIniousAuthPage
 class CustomValuePage(INGIniousAuthPage):
     def GET_AUTH(self, courseid):
         """ GET request """
-        course = self.course_factory.get_course(courseid)
-        course_content = self.course_factory.get_course_descriptor_content(courseid)
-        custom_fields = course_content["fields"] if "fields" in course_content else {}
-
-        return self.template_helper.render("custom_values.html", course=course, custom_fields=custom_fields)
+        username = self.user_manager.session_username()
+        current_user = self.database.users.find_one(
+            {"username": username})
+        custom_values_user = current_user["custom"] if "custom" in current_user else {}
+        return self.show_page(courseid, custom_values_user)
 
     def POST_AUTH(self, courseid):
         """ POST request """
-        course = self.course_factory.get_course(courseid)
-
         user_input = flask.request.form
-        print(user_input)
-        return self.template_helper.render("maintenance.html")
+        username = self.user_manager.session_username()
+        current_user = self.database.users.find_one(
+            {"username": username})
+        custom_values_user = current_user["custom"] if "custom" in current_user else {}
+        for key in user_input:
+            custom_values_user[key] = user_input[key]
+        removed_keys = list(set(custom_values_user.keys()) - set(user_input.keys()))
+        for key in removed_keys:
+            custom_values_user[key] = "off"
+        self.database.users.update_one({"username": username}, {"$set": {"custom": custom_values_user}})
+
+        return self.show_page(courseid, custom_values_user)
+
+    def show_page(self, courseid, custom_values_user):
+        course = self.course_factory.get_course(courseid)
+        course_content = self.course_factory.get_course_descriptor_content(courseid)
+        custom_fields = course_content["fields"] if "fields" in course_content else {}
+        return self.template_helper.render("custom_values.html", course=course, custom_fields=custom_fields,
+                                           custom_values_user=custom_values_user)
 
