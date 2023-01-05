770b4b0a1fd0bc83fe2af4c20a2fb987474d5359
Ludovic Taffin <ludovic.taffin@uclouvain.be>
2023-01-05 11:55:43 +0100

    Make feedback more explicit.

--- inginious/frontend/pages/course_user_settings.py
+++ inginious/frontend/pages/course_user_settings.py
@@ -24,7 +24,7 @@ class CourseUserSettingPage(INGIniousAuthPage):
         current_user = self.database.users.find_one(
             {"username": username})
         course_user_settings = current_user.get("course_settings", {})
-        return self.show_page(courseid, course_user_settings.get(courseid, {}))
+        return self.show_page(courseid, course_user_settings.get(courseid, {}), None)
 
     def POST_AUTH(self, courseid):
         """ POST request """
@@ -32,20 +32,32 @@ class CourseUserSettingPage(INGIniousAuthPage):
         if not self._is_accessible(courseid, username):
             return self.template_helper.render("course_unavailable.html")
         course_user_settings = self._sanitize_content(flask.request.form, courseid)
-        if "error" in course_user_settings:
-            return self.show_page(courseid, course_user_settings)
+        if type(course_user_settings)==str:
+            feedback = ("danger",course_user_settings)
+            current_user = self.database.users.find_one(
+                {"username": username})
+            course_user_settings = current_user.get("course_settings", {})
+            return self.show_page(courseid, course_user_settings.get(courseid, {}), feedback)
+
         self.database.users.update_one({"username": username}, {"$set": {"course_settings." + courseid: course_user_settings}})
 
-        return self.show_page(courseid, course_user_settings)
+        return self.show_page(courseid, course_user_settings, ("success","Course settings successfully updated."))
 
-    def show_page(self, courseid, course_user_settings):
-        """Definition of the show page method."""
+    def show_page(self, courseid, course_user_settings, feedback):
+        """
+            Definition of the show page method.
+            :param: courseid: the id of the course.
+            :param: course_user_settings: The dict with the settings values.
+            :param: feedback: a tuple with the type of feedback and the feedback. None if there is no feedback.
+            :return:
+        """
         try:
             course = self.course_factory.get_course(courseid)
             course_user_setting_fields = course.get_course_user_settings()
             return self.template_helper.render("course_user_settings.html", course=course,
                                                course_user_setting_fields=course_user_setting_fields,
-                                               course_user_settings=course_user_settings, fieldtypes=FieldTypes)
+                                               course_user_settings=course_user_settings, fieldtypes=FieldTypes,
+                                               feedback=feedback)
         except Exception:
             return self.template_helper.render("course_unavailable.html")
 
@@ -58,9 +70,7 @@ class CourseUserSettingPage(INGIniousAuthPage):
         """
         try:
             course = self.course_factory.get_course(courseid)
-            if not self.user_manager.course_is_user_registered(course, username):
-                return False
-            return True
+            return self.user_manager.course_is_user_registered(course, username)
         except Exception:
             return False
 
@@ -69,20 +79,19 @@ class CourseUserSettingPage(INGIniousAuthPage):
             Sanitize received data
             :param: data  - A dictionary that normally contains user settings for a course.
             :param: courseid - Id of the course.
-            :return: A sanitized dict, an empty dict if something went wrong.
+            :return: A sanitized dict or a str feedback if something went wrong.
         """
         if not isinstance(data, dict):
-            return {"error":"Incorrect type of data."}
+            return "Incorrect type of data."
         copied_data = {}
         course = self.course_factory.get_course(courseid)
         add_fields = course.get_course_user_settings()
         for setting in data:
             if setting not in add_fields:
                 # if setting is not expected in the course definition. There is no reason to treat it.
-                return {"error":"Unknown field: "+str(setting)}
-            casted_value = None
+                return "Unknown field: "+str(setting)
             try:
-                # we try to cast given value to be sure that we match expected type.
+                # try to cast given value to be sure that we match expected type.
                 if add_fields[setting].get_type_name() == "STRING":
                     casted_value= str(data[setting])
                 elif add_fields[setting].get_type_name() == "INTEGER":
@@ -90,6 +99,6 @@ class CourseUserSettingPage(INGIniousAuthPage):
                 else:
                     casted_value = bool(data[setting])
             except ValueError:
-                return {"error": "Wrong value for field: " + str(setting)}
+                return "Wrong value for field: " + str(setting)
             copied_data[setting] = casted_value
         return copied_data
