72bb543fb38d0dd5988fda612ce20d35d15f6656
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2024-01-26 14:21:13 +0100

    [frontend/constest] Fixing no soft_end given in contest .get_accessibility()

--- inginious/frontend/plugins/contests/__init__.py
+++ inginious/frontend/plugins/contests/__init__.py
@@ -57,7 +57,7 @@ class Contest(TableOfContents):
     def get_accessibilities(self, taskids, usernames): # pylint: disable=unused-argument
         contest_data = self.get_contest_data()
         if contest_data['enabled']:
-            accessibility = {"start": contest_data['start'], "end": contest_data['end']}
+            accessibility = {"start": contest_data['start'], "soft_end": contest_data['end'], "end": contest_data['end']}
             return {username: {taskid: AccessibleTime(accessibility) for taskid in taskids} for username in usernames}
         else:
             return TableOfContents.get_accessibilities(self, taskids, usernames)
