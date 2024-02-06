66cd9c1bf4ccd8b74408097e1c19a0213d1e2fe3
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2024-02-06 16:33:17 +0100

    [frontend/combinatory_test] Removing obsolete code
    The structure was changed in commit 750aab6 but only task dispenser toc was modified and changes for combinatory test were forgotten.

--- inginious/frontend/task_dispensers/combinatory_test.py
+++ inginious/frontend/task_dispensers/combinatory_test.py
@@ -52,8 +52,7 @@ class CombinatoryTest(TableOfContents):
 
         return template_helper.render("task_dispensers_admin/combinatory_test.html",  element=element, course=course,
                                       taskset=taskset, dispenser_structure=self._toc, dispenser_config=self._task_config,
-                                      tasks=task_data, task_errors=task_errors, config_fields=config_fields,
-                                      config_items_funcs=["dispenser_util_get_" + config_item.get_id() for config_item in self.config_items])
+                                      tasks=task_data, task_errors=task_errors, config_fields=config_fields)
 
     def render(self, template_helper, course, tasks_data, tag_list, username):
         """ Returns the formatted task list"""
